d4fcd9d43b9f7115a90735abc8a8599eb9af2b73
thedavidmeister <thedavidmeister@gmail.com>
2021-06-22 00:36:02 +0400

    more tests

--- test/erc20.ts
+++ test/erc20.ts
@@ -1,7 +1,7 @@
 import chai from 'chai'
 import { solidity } from 'ethereum-waffle'
 import { ethers } from 'hardhat'
-import { deployEthGild, expectedName, expectedSymbol } from './util'
+import { deployEthGild, expectedName, expectedReferencePrice, expectedSymbol } from './util'
 import type { EthGild } from '../typechain/EthGild'
 
 chai.use(solidity)
@@ -16,5 +16,55 @@ describe('erc20 usage', async function() {
 
         assert(erc20Name === expectedName, 'erc20 did not construct with correct name')
         assert(erc20Symbol === expectedSymbol, 'erc20 did not construct with correct symbol')
+    })
+
+    it('should only send itself', async function() {
+        const signers = await ethers.getSigners()
+
+        const ethGild = await deployEthGild() as EthGild
+
+        await ethGild.gild({value: 1000})
+
+        const expectedErc20Balance = ethers.BigNumber.from('1260')
+        const expectedErc20BalanceAfter = ethers.BigNumber.from('630')
+        const expectedErc1155Balance = ethers.BigNumber.from('1260')
+        const expectedErc1155BalanceAfter = ethers.BigNumber.from('1260')
+
+        const erc20Balance = await ethGild['balanceOf(address)'](signers[0].address)
+        assert(
+            erc20Balance.eq(expectedErc20Balance),
+            `wrong erc20 balance ${expectedErc20Balance} ${erc20Balance}`
+        )
+
+        const erc1155Balance = await ethGild['balanceOf(address,uint256)'](signers[0].address, expectedReferencePrice)
+        assert(
+            erc1155Balance.eq(expectedErc1155Balance),
+            `wrong erc1155 balance ${expectedErc20Balance} ${erc1155Balance}`
+        )
+
+        await ethGild.transfer(signers[1].address, '630')
+
+        const erc20BalanceAfter = await ethGild['balanceOf(address)'](signers[0].address)
+        assert(
+            erc20BalanceAfter.eq(expectedErc20BalanceAfter),
+            `wrong erc20 balance after ${expectedErc20BalanceAfter} ${erc20BalanceAfter}`
+        )
+
+        const erc20BalanceAfter2 = await ethGild['balanceOf(address)'](signers[1].address)
+        assert(
+            erc20BalanceAfter2.eq(expectedErc20BalanceAfter),
+            `wrong erc20 balance after 2 ${expectedErc20BalanceAfter} ${erc20BalanceAfter2}`
+        )
+
+        const erc1155BalanceAfter = await ethGild['balanceOf(address,uint256)'](signers[0].address, expectedReferencePrice)
+        assert(
+            erc1155BalanceAfter.eq(expectedErc20Balance),
+            `wrong erc1155 balance after ${expectedErc1155BalanceAfter} ${erc1155BalanceAfter}`
+        )
+
+        assert(
+            (await ethGild['balanceOf(address,uint256)'](signers[1].address, expectedReferencePrice)).eq(0),
+            `wrong erc1155 balance 2 after ${expectedErc1155BalanceAfter} ${erc1155BalanceAfter}`
+        )
     })
 })
