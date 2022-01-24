bd6a154d2cce7093185321493d0dc113fa2e451e
thedavidmeister <thedavidmeister@gmail.com>
2022-01-24 13:10:37 +0400

    add mock oracle

--- contracts/ethgild.sol
+++ contracts/ethgild.sol
@@ -226,6 +226,9 @@ struct OracleConfig {
 /// never be more secure than Chainlink itself, if their oracles are damaged
 /// somehow then EthGild suffers too.
 contract EthGild is ERC1155, ERC20 {
+    /// Sender asserts that there is a URI to interact with ETHGild on.
+    /// The URI may be malicious and steal funds so end-users are strongly
+    /// encouraged to carefully consider their reasons for trusting `sender`.
     event AltURI(
         /// `msg.sender` logging the new alt URI.
         address sender,
@@ -285,6 +288,11 @@ contract EthGild is ERC1155, ERC20 {
         chainlinkEthUsd = oracleConfig_.chainlinkEthUsd;
     }
 
+    /// Log an alternative URI that the sender claims is handling `EthGild`.
+    /// The URI may trivially be malicious so end users are strongly encouraged
+    /// to carefully consider their reasons for trusting `msg.sender` to not
+    /// steal funds.
+    /// @param altUri_ The alternative URI for `EthGild` 1155 tokens.
     function altURI(string calldata altUri_) external {
         emit AltURI(msg.sender, altUri_);
     }
