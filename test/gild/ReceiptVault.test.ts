a6c025ef2a85f12dd52fc85a9f80e6df561f7f6e
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-07-04 17:16:22 +0400

    Test correct withdrawId is set

--- test/gild/ReceiptVault.test.ts
+++ test/gild/ReceiptVault.test.ts
@@ -15,27 +15,40 @@ chai.use(solidity);
 
 const {assert} = chai;
 
-describe("config", async function () {
+describe("Receipt vault", async function () {
   it("Returns the address of the underlying asset that is deposited", async function () {
     const [vault, asset] = await deployERC20PriceOracleVault();
     const vaultAsset = await vault.asset();
 
     assert(
       vaultAsset === asset.address,
-      "Wrong asset address"
+      `Wrong asset address ${asset.address} ${vaultAsset}`
     );
   }),
     it("Sets the correct min Share Ratio", async function () {
       [owner] = await ethers.getSigners()
       const expectedMinShareRatio = 100
 
-      const [vault, asset] = await deployERC20PriceOracleVault();
+      const [vault] = await deployERC20PriceOracleVault();
       await vault.setMinShareRatio(100)
       let minShareRatio = await vault.minShareRatios(owner.address)
 
       assert(
         minShareRatio.toNumber() === expectedMinShareRatio,
-        "Wrong min Share Ratio"
+        `Wrong min Share Ratio ${expectedMinShareRatio} ${minShareRatio.toNumber()}`
+      );
+    }),
+    it("Sets the correct withdraw Id", async function () {
+      [owner] = await ethers.getSigners()
+      const expectedWithdrawId = 100
+
+      const [vault] = await deployERC20PriceOracleVault();
+      await vault.setWithdrawId(100)
+      let withdrawId = await vault.withdrawIds(owner.address)
+
+      assert(
+        withdrawId.toNumber() === expectedWithdrawId,
+        `Wrong withdraw Id ${expectedWithdrawId} ${withdrawId.toNumber()}`
       );
     })
 })
