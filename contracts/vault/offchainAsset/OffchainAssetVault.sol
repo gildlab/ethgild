78310e57e7bf50ce7c1638fe59f07cdeedde1fc7
thedavidmeister <thedavidmeister@gmail.com>
2022-07-14 17:17:40 +0400

    public roles

--- contracts/vault/offchainAsset/OffchainAssetVault.sol
+++ contracts/vault/offchainAsset/OffchainAssetVault.sol
@@ -115,31 +115,31 @@ contract OffchainAssetVault is ReceiptVault, AccessControl {
     /// to send/receive/hold receipts and be immune to receipt confiscations.
     event SetERC1155Tier(address caller, address tier, uint minimumTier);
 
-    bytes32 private constant DEPOSITOR = keccak256("DEPOSITOR");
-    bytes32 private constant DEPOSITOR_ADMIN = keccak256("DEPOSITOR_ADMIN");
+    bytes32 public constant DEPOSITOR = keccak256("DEPOSITOR");
+    bytes32 public constant DEPOSITOR_ADMIN = keccak256("DEPOSITOR_ADMIN");
 
-    bytes32 private constant WITHDRAWER = keccak256("WITHDRAWER");
-    bytes32 private constant WITHDRAWER_ADMIN = keccak256("WITHDRAWER_ADMIN");
+    bytes32 public constant WITHDRAWER = keccak256("WITHDRAWER");
+    bytes32 public constant WITHDRAWER_ADMIN = keccak256("WITHDRAWER_ADMIN");
 
-    bytes32 private constant CERTIFIER = keccak256("CERTIFIER");
-    bytes32 private constant CERTIFIER_ADMIN = keccak256("CERTIFIER_ADMIN");
+    bytes32 public constant CERTIFIER = keccak256("CERTIFIER");
+    bytes32 public constant CERTIFIER_ADMIN = keccak256("CERTIFIER_ADMIN");
 
-    bytes32 private constant HANDLER = keccak256("HANDLER");
-    bytes32 private constant HANDLER_ADMIN = keccak256("HANDLER_ADMIN");
+    bytes32 public constant HANDLER = keccak256("HANDLER");
+    bytes32 public constant HANDLER_ADMIN = keccak256("HANDLER_ADMIN");
 
-    bytes32 private constant ERC20TIERER = keccak256("ERC20TIERER");
-    bytes32 private constant ERC20TIERER_ADMIN = keccak256("ERC20TIERER_ADMIN");
+    bytes32 public constant ERC20TIERER = keccak256("ERC20TIERER");
+    bytes32 public constant ERC20TIERER_ADMIN = keccak256("ERC20TIERER_ADMIN");
 
-    bytes32 private constant ERC1155TIERER = keccak256("ERC1155TIERER");
-    bytes32 private constant ERC1155TIERER_ADMIN =
+    bytes32 public constant ERC1155TIERER = keccak256("ERC1155TIERER");
+    bytes32 public constant ERC1155TIERER_ADMIN =
         keccak256("ERC1155TIERER_ADMIN");
 
-    bytes32 private constant ERC20SNAPSHOTTER = keccak256("ERC20SNAPSHOTTER");
-    bytes32 private constant ERC20SNAPSHOTTER_ADMIN =
+    bytes32 public constant ERC20SNAPSHOTTER = keccak256("ERC20SNAPSHOTTER");
+    bytes32 public constant ERC20SNAPSHOTTER_ADMIN =
         keccak256("ERC20SNAPSHOTTER_ADMIN");
 
-    bytes32 private constant CONFISCATOR = keccak256("CONFISCATOR");
-    bytes32 private constant CONFISCATOR_ADMIN = keccak256("CONFISCATOR_ADMIN");
+    bytes32 public constant CONFISCATOR = keccak256("CONFISCATOR");
+    bytes32 public constant CONFISCATOR_ADMIN = keccak256("CONFISCATOR_ADMIN");
 
     uint256 private highwaterId;
 
