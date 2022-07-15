eb29b8eddbc739acdeba6ee6edcc575a3d24356d
thedavidmeister <thedavidmeister@gmail.com>
2022-07-15 11:52:06 +0400

    lint

--- contracts/test/TierV2Test.sol
+++ contracts/test/TierV2Test.sol
@@ -1,27 +1,4 @@
-// SPDX-License-Identifier: CAL
+// SPDX-License-Identifier: MIT
 pragma solidity =0.8.10;
 
 import "@beehiveinnovation/rain-protocol/contracts/test/ReadWriteTier.sol";
-
-// import {TierConstants} from "@beehiveinnovation/rain-protocol/contracts/tier/libraries/TierConstants.sol";
-
-// /// @title TierV2Test
-// contract TierV2Test is ITierV2 {
-//     /// Either fetch the report from storage or return UNINITIALIZED.
-//     function report(address, uint256[] memory)
-//         public
-//         view
-//         virtual
-//         returns (uint256)
-//     {
-//         return TierConstants.NEVER_REPORT;
-//     }
-
-//     function reportTimeForTier(
-//         address,
-//         uint256,
-//         uint256[] calldata
-//     ) external view returns (uint256) {
-//         return block.timestamp;
-//     }
-// }
