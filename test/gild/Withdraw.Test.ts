a2a5bdc138406cbf614d80762fd81636f49920bf
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-07-19 13:06:30 +0400

    Check correct maxwithdraw is set

--- test/gild/Withdraw.Test.ts
+++ test/gild/Withdraw.Test.ts
@@ -1,16 +1,46 @@
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
 import { ethers } from "hardhat";
-import {
-  deployERC20PriceOracleVault,
-  fixedPointDiv,
-} from "../util";
+import { deployERC20PriceOracleVault, fixedPointDiv } from "../util";
 
 chai.use(solidity);
 
 const { assert } = chai;
 
 describe("Withdraw", async function () {
+  it("Calculates correct maxWithdraw", async function () {
+    const signers = await ethers.getSigners();
+    const alice = signers[0];
+
+    const [vault, asset, priceOracle] = await deployERC20PriceOracleVault();
+
+    const price = await priceOracle.price();
+
+    const aliceAssets = ethers.BigNumber.from(5000);
+    await asset.transfer(alice.address, aliceAssets);
+
+    await asset.connect(alice).increaseAllowance(vault.address, aliceAssets);
+
+    const depositTx = await vault["deposit(uint256,address,uint256,bytes)"](
+      aliceAssets,
+      alice.address,
+      price,
+      []
+    );
+
+    await depositTx.wait();
+    const receiptBalance = await vault["balanceOf(address,uint256)"](
+      alice.address,
+      price
+    );
+
+    const expectedMaxWithdraw = fixedPointDiv(receiptBalance, price);
+    await vault.setWithdrawId(price);
+
+    const maxWithdraw = await vault["maxWithdraw(address)"](alice.address);
+
+    assert(maxWithdraw.eq(expectedMaxWithdraw), `Wrong max withdraw amount`);
+  });
   it("Withdraws", async function () {
     const signers = await ethers.getSigners();
     const alice = signers[0];
