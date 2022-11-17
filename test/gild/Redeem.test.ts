d441037ca5ecfb6ad2190d17959bf423bc6253c7
Nino Keldishvili <Ninokeldishvili@gmail.com>
2022-11-17 15:08:15 +0400

    Fix redeem test

--- test/gild/Redeem.test.ts
+++ test/gild/Redeem.test.ts
@@ -5,14 +5,12 @@ import {
   assertError,
   deployERC20PriceOracleVault,
   fixedPointDiv,
-  fixedPointMul,
   ADDRESS_ZERO,
-  getEventArgs,
   getEvent,
 } from "../util";
 import {
   ERC20Upgradeable as ERC20,
-  ERC20PriceOracleReceiptVault,
+  ERC20PriceOracleReceiptVault, Receipt,
 } from "../../typechain";
 import { BigNumber } from "ethers";
 import { WithdrawEvent } from "../../typechain/IERC4626";
@@ -25,19 +23,22 @@ let vault: ERC20PriceOracleReceiptVault,
   asset: ERC20,
   shareRatio: BigNumber,
   aliceAddress: string,
-  aliceAssets: BigNumber;
+  aliceAssets: BigNumber,
+  receipt: Receipt;
 
 describe("Redeem", async function () {
+  let alice;
   beforeEach(async () => {
     const signers = await ethers.getSigners();
-    const alice = signers[0];
+    alice = signers[0];
 
-    const [ERC20PriceOracleVault, Erc20Asset, priceOracle] =
+    const [ERC20PriceOracleVault, Erc20Asset, priceOracle, receiptVault] =
       await deployERC20PriceOracleVault();
 
     vault = await ERC20PriceOracleVault;
     asset = await Erc20Asset;
     shareRatio = await priceOracle.price();
+    receipt = await receiptVault
     aliceAddress = alice.address;
 
     aliceAssets = ethers.BigNumber.from(5000);
@@ -45,7 +46,7 @@ describe("Redeem", async function () {
 
     await asset.connect(alice).increaseAllowance(vault.address, aliceAssets);
 
-    const depositTx = await vault["deposit(uint256,address,uint256,bytes)"](
+    const depositTx = await vault.connect(alice)["deposit(uint256,address,uint256,bytes)"](
       aliceAssets,
       aliceAddress,
       shareRatio,
@@ -55,24 +56,24 @@ describe("Redeem", async function () {
     await depositTx.wait();
   });
   it("Calculates correct maxRedeem", async function () {
-    const expectedMaxRedeem = await vault["balanceOf(address,uint256)"](
+    const expectedMaxRedeem = await receipt.connect(alice)["balanceOf(address,uint256)"](
       aliceAddress,
       shareRatio
     );
-    await vault.setWithdrawId(shareRatio);
-    const maxRedeem = await vault["maxRedeem(address)"](aliceAddress);
+    await vault.connect(alice).setWithdrawId(shareRatio);
+    const maxRedeem = await vault.connect(alice)["maxRedeem(address)"](aliceAddress);
     assert(
       maxRedeem.eq(expectedMaxRedeem),
       `Wrong max withdraw amount expected ${expectedMaxRedeem} got ${maxRedeem}`
     );
   });
   it("Overloaded maxRedeem - Calculates correct maxRedeem", async function () {
-    const expectedMaxRedeem = await vault["balanceOf(address,uint256)"](
+    const expectedMaxRedeem = await receipt.connect(alice)["balanceOf(address,uint256)"](
       aliceAddress,
       shareRatio
     );
 
-    const maxRedeem = await vault["maxRedeem(address,uint256)"](
+    const maxRedeem = await vault.connect(alice)["maxRedeem(address,uint256)"](
       aliceAddress,
       shareRatio
     );
@@ -83,32 +84,32 @@ describe("Redeem", async function () {
     );
   });
   it("previewRedeem - calculates correct assets", async function () {
-    const aliceReceiptBalance = await vault["balanceOf(address,uint256)"](
+    const aliceReceiptBalance = await receipt.connect(alice)["balanceOf(address,uint256)"](
       aliceAddress,
       shareRatio
     );
 
-    await vault.setWithdrawId(shareRatio);
+    await vault.connect(alice).setWithdrawId(shareRatio);
     const expectedPreviewRedeem = fixedPointDiv(
       aliceReceiptBalance,
       shareRatio
     );
 
-    const assets = await vault["previewRedeem(uint256)"](aliceReceiptBalance);
+    const assets = await vault.connect(alice)["previewRedeem(uint256)"](aliceReceiptBalance);
     assert(
       assets.eq(expectedPreviewRedeem),
       `Wrong asset amount expected ${expectedPreviewRedeem} got ${assets}`
     );
   });
   it("Overloaded previewRedeem - calculates correct assets", async function () {
-    const aliceReceiptBalance = await vault["balanceOf(address,uint256)"](
+    const aliceReceiptBalance = await receipt.connect(alice)["balanceOf(address,uint256)"](
       aliceAddress,
       shareRatio
     );
 
     const expectedAssets = fixedPointDiv(aliceReceiptBalance, shareRatio);
 
-    const assets = await vault["previewRedeem(uint256,uint256)"](
+    const assets = await vault.connect(alice)["previewRedeem(uint256,uint256)"](
       aliceReceiptBalance,
       shareRatio
     );
@@ -118,19 +119,19 @@ describe("Redeem", async function () {
     );
   });
   it("Redeems", async function () {
-    const receiptBalance = await vault["balanceOf(address,uint256)"](
+    const receiptBalance = await receipt.connect(alice)["balanceOf(address,uint256)"](
       aliceAddress,
       shareRatio
     );
 
-    await vault.setWithdrawId(shareRatio);
-    await vault["redeem(uint256,address,address)"](
+    await vault.connect(alice).setWithdrawId(shareRatio);
+    await vault.connect(alice)["redeem(uint256,address,address)"](
       receiptBalance,
       aliceAddress,
       aliceAddress
     );
 
-    const receiptBalanceAfter = await vault["balanceOf(address,uint256)"](
+    const receiptBalanceAfter = await receipt.connect(alice)["balanceOf(address,uint256)"](
       aliceAddress,
       shareRatio
     );
@@ -140,11 +141,11 @@ describe("Redeem", async function () {
     );
   });
   it("Should not redeem on zero assets", async function () {
-    await vault.setWithdrawId(shareRatio);
+    await vault.connect(alice).setWithdrawId(shareRatio);
 
     await assertError(
       async () =>
-        await vault["redeem(uint256,address,address)"](
+        await vault.connect(alice)["redeem(uint256,address,address)"](
           ethers.BigNumber.from(0),
           aliceAddress,
           aliceAddress
@@ -154,16 +155,16 @@ describe("Redeem", async function () {
     );
   });
   it("Should not redeem on zero address receiver", async function () {
-    const receiptBalance = await vault["balanceOf(address,uint256)"](
+    const receiptBalance = await receipt.connect(alice)["balanceOf(address,uint256)"](
       aliceAddress,
       shareRatio
     );
 
-    await vault.setWithdrawId(shareRatio);
+    await vault.connect(alice).setWithdrawId(shareRatio);
 
     await assertError(
       async () =>
-        await vault["redeem(uint256,address,address)"](
+        await vault.connect(alice)["redeem(uint256,address,address)"](
           receiptBalance,
           ADDRESS_ZERO,
           aliceAddress
@@ -173,15 +174,15 @@ describe("Redeem", async function () {
     );
   });
   it("Should emit withdraw event", async function () {
-    const receiptBalance = await vault["balanceOf(address,uint256)"](
+    const receiptBalance = await receipt.connect(alice)["balanceOf(address,uint256)"](
       aliceAddress,
       shareRatio
     );
-    await vault.setWithdrawId(shareRatio);
+    await vault.connect(alice).setWithdrawId(shareRatio);
 
     const expectedAssets = fixedPointDiv(receiptBalance, shareRatio);
 
-    const redeemTx = await vault["redeem(uint256,address,address)"](
+    const redeemTx = await vault.connect(alice)["redeem(uint256,address,address)"](
       receiptBalance,
       aliceAddress,
       aliceAddress
