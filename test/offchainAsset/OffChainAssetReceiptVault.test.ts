dc87701113ead256685d490d4a92431211c772be
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-01-19 16:57:12 +0400

    Refactor

--- test/offchainAsset/OffChainAssetReceiptVault.test.ts
+++ test/offchainAsset/OffChainAssetReceiptVault.test.ts
@@ -24,7 +24,6 @@ import {
   deployOffchainAssetReceiptVaultFactory,
 } from "./deployOffchainAssetReceiptVault";
 import { DepositWithReceiptEvent } from "../../typechain-types/contracts/vault/receipt/ReceiptVault";
-import { max } from "hardhat/internal/util/bigint";
 
 const assert = require("assert");
 
@@ -286,6 +285,39 @@ describe("OffChainAssetReceiptVault", async function () {
     assert(
       shares.eq(expectedShares),
       `Wrong shares: expected ${expectedShares} got ${shares} `
+    );
+  });
+  it("Mints with data", async function () {
+    const [vault, receipt] = await deployOffChainAssetReceiptVault();
+    const signers = await ethers.getSigners();
+    const alice = signers[0];
+
+    const testErc20 = await ethers.getContractFactory("TestErc20");
+    const asset = (await testErc20.deploy()) as TestErc20;
+    await asset.deployed();
+
+    await vault
+      .connect(alice)
+      .grantRole(await vault.connect(alice).DEPOSITOR(), alice.address);
+
+    const assets = ethers.BigNumber.from(5000);
+    await asset.transfer(alice.address, assets);
+    await asset.connect(alice).increaseAllowance(vault.address, assets);
+
+    const shares = fixedPointMul(assets, ONE).add(1);
+
+    await vault
+      .connect(alice)
+      ["mint(uint256,address,uint256,bytes)"](shares, alice.address, ONE, [1]);
+
+    const expectedAssets = fixedPointDiv(shares, ONE);
+    const aliceBalanceAfter = await receipt
+      .connect(alice)
+      ["balanceOf(address,uint256)"](alice.address, 1);
+
+    assert(
+      aliceBalanceAfter.eq(expectedAssets),
+      `wrong assets. expected ${expectedAssets} got ${aliceBalanceAfter}`
     );
   });
   it("PreviewRedeem returns correct assets", async function () {
--- test/receipt/Receipt.test.ts
+++ test/receipt/Receipt.test.ts
@@ -1,14 +1,5 @@
 import { artifacts, ethers } from "hardhat";
-import {
-  ADDRESS_ZERO,
-  assertError,
-  expectedUri,
-  fixedPointDiv,
-  fixedPointMul,
-  getEventArgs,
-  ONE,
-} from "../util";
-import { deployOffChainAssetReceiptVault } from "../offchainAsset/deployOffchainAssetReceiptVault";
+import { ADDRESS_ZERO, assertError, expectedUri, getEventArgs } from "../util";
 import {
   Receipt,
   ReceiptFactory,
@@ -21,39 +12,6 @@ import { Contract } from "ethers";
 const assert = require("assert");
 
 describe("Receipt vault", async function () {
-  it("Mints with data", async function () {
-    const [vault, receipt] = await deployOffChainAssetReceiptVault();
-    const signers = await ethers.getSigners();
-    const alice = signers[0];
-
-    const testErc20 = await ethers.getContractFactory("TestErc20");
-    const asset = (await testErc20.deploy()) as TestErc20;
-    await asset.deployed();
-
-    await vault
-      .connect(alice)
-      .grantRole(await vault.connect(alice).DEPOSITOR(), alice.address);
-
-    const assets = ethers.BigNumber.from(5000);
-    await asset.transfer(alice.address, assets);
-    await asset.connect(alice).increaseAllowance(vault.address, assets);
-
-    const shares = fixedPointMul(assets, ONE).add(1);
-
-    await vault
-      .connect(alice)
-      ["mint(uint256,address,uint256,bytes)"](shares, alice.address, ONE, [1]);
-
-    const expectedAssets = fixedPointDiv(shares, ONE);
-    const aliceBalanceAfter = await receipt
-      .connect(alice)
-      ["balanceOf(address,uint256)"](alice.address, 1);
-
-    assert(
-      aliceBalanceAfter.eq(expectedAssets),
-      `wrong assets. expected ${expectedAssets} got ${aliceBalanceAfter}`
-    );
-  });
   it("Initializes with factory", async function () {
     const signers = await ethers.getSigners();
     const alice = signers[0];
