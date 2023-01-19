5c552065ae748afcc9ea038ebaf34feea38da729
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-01-19 15:31:00 +0400

    OwnerTransferFrom transforms balances

--- test/receipt/Receipt.test.ts
+++ test/receipt/Receipt.test.ts
@@ -229,9 +229,6 @@ describe("Receipt vault", async function () {
 
     await receipt.setOwner(receiptOwner.address);
 
-    await receiptOwner.setFrom(ADDRESS_ZERO);
-    await receiptOwner.setTo(alice.address);
-
     const assets = ethers.BigNumber.from(30);
     await asset.transfer(alice.address, assets);
     await asset.connect(alice).increaseAllowance(receiptOwner.address, assets);
@@ -258,7 +255,7 @@ describe("Receipt vault", async function () {
       "failed to prevent ownerBurn"
     );
   });
-  it.only("OwnerTransferFrom more than balance", async function () {
+  it("OwnerTransferFrom more than balance", async function () {
     const signers = await ethers.getSigners();
     const alice = signers[0];
     const bob = signers[1];
@@ -300,6 +297,68 @@ describe("Receipt vault", async function () {
                 .ownerTransferFrom(receipt.address, alice.address, bob.address, receiptId, transferAmount, []),
         "ERC1155: insufficient balance for transfer",
         "failed to transfer"
+    );
+  });
+  it("OwnerTransferFrom transforms balances", async function () {
+    const signers = await ethers.getSigners();
+    const alice = signers[0];
+    const bob = signers[1];
+
+    const testErc20 = await ethers.getContractFactory("TestErc20");
+    const asset = (await testErc20.deploy()) as TestErc20;
+    await asset.deployed();
+
+    const testReceipt = await ethers.getContractFactory("TestReceipt");
+    const receipt = (await testReceipt.deploy()) as TestReceipt;
+    await receipt.deployed();
+
+    const testReceiptOwner = await ethers.getContractFactory(
+        "TestReceiptOwner"
+    );
+    const receiptOwner = (await testReceiptOwner.deploy()) as TestReceiptOwner;
+    await receiptOwner.deployed();
+
+    await receipt.setOwner(receiptOwner.address);
+
+    await receiptOwner.setFrom(ADDRESS_ZERO);
+    await receiptOwner.setTo(alice.address);
+
+    const assets = ethers.BigNumber.from(30);
+    await asset.transfer(alice.address, assets);
+    await asset.connect(alice).increaseAllowance(receiptOwner.address, assets);
+
+    const receiptId = ethers.BigNumber.from(1);
+    const transferAmount = ethers.BigNumber.from(10);
+
+    await receiptOwner
+        .connect(alice)
+        .ownerMint(receipt.address, alice.address, receiptId, transferAmount, []);
+
+    const balanceBefore = await receipt.balanceOf(alice.address, receiptId);
+    const balanceBeforeBob = await receipt.balanceOf(bob.address, receiptId);
+
+    await receiptOwner.setFrom(alice.address);
+    await receiptOwner.setTo(bob.address);
+
+    await receiptOwner
+        .connect(alice)
+        .ownerTransferFrom(receipt.address, alice.address, bob.address, receiptId, transferAmount, []);
+
+    const balanceAfter = await receipt.balanceOf(alice.address, receiptId);
+    const balanceAfterBob = await receipt.balanceOf(bob.address, receiptId);
+
+    assert(
+        balanceAfterBob.eq(balanceBeforeBob.add(transferAmount)),
+        `Wrong balance for bob. Expected ${balanceBeforeBob.add(
+            transferAmount
+        )}, got ${balanceAfterBob}`
+    );
+
+    assert(
+        balanceAfter.eq(balanceBefore.sub(transferAmount)),
+        `Wrong balance for alice. Expected ${balanceBefore.sub(
+            transferAmount
+        )}, got ${balanceAfter}`
     );
   });
 });
