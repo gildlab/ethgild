6011ef9d72d5b0ea7f8450fa5513070c2146acf5
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-01-19 15:24:17 +0400

    OwnertransferFrom more than balance

--- test/receipt/Receipt.test.ts
+++ test/receipt/Receipt.test.ts
@@ -258,4 +258,48 @@ describe("Receipt vault", async function () {
       "failed to prevent ownerBurn"
     );
   });
+  it.only("OwnerTransferFrom more than balance", async function () {
+    const signers = await ethers.getSigners();
+    const alice = signers[0];
+    const bob = signers[1];
+
+    const testErc20 = await ethers.getContractFactory("TestErc20");
+    const asset = (await testErc20.deploy()) as TestErc20;
+    await asset.deployed();
+
+    const testReceipt = await ethers.getContractFactory("TestReceipt");
+    const receipt = (await testReceipt.deploy()) as TestReceipt;
+    await receipt.deployed();
+
+    const testReceiptOwner = await ethers.getContractFactory(
+        "TestReceiptOwner"
+    );
+    const receiptOwner = (await testReceiptOwner.deploy()) as TestReceiptOwner;
+    await receiptOwner.deployed();
+
+    await receipt.setOwner(receiptOwner.address);
+
+    await receiptOwner.setFrom(ADDRESS_ZERO);
+    await receiptOwner.setTo(alice.address);
+
+    const assets = ethers.BigNumber.from(30);
+    await asset.transfer(alice.address, assets);
+    await asset.connect(alice).increaseAllowance(receiptOwner.address, assets);
+
+    const receiptId = ethers.BigNumber.from(1);
+
+    const transferAmount = ethers.BigNumber.from(10);
+
+    await receiptOwner.setFrom(alice.address);
+    await receiptOwner.setTo(bob.address);
+
+    await assertError(
+        async () =>
+            await receiptOwner
+                .connect(alice)
+                .ownerTransferFrom(receipt.address, alice.address, bob.address, receiptId, transferAmount, []),
+        "ERC1155: insufficient balance for transfer",
+        "failed to transfer"
+    );
+  });
 });
