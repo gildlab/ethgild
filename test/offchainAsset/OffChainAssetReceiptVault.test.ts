b2254498847f9c0efa7da845e76c9969b9e76bee
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-01-23 14:54:56 +0400

    SnapshotWithData event is emitted

--- test/offchainAsset/OffChainAssetReceiptVault.test.ts
+++ test/offchainAsset/OffChainAssetReceiptVault.test.ts
@@ -18,6 +18,7 @@ import {
   ConfiscateSharesEvent,
   ConfiscateReceiptEvent,
   SetERC1155TierEvent,
+  SnapshotWithDataEvent,
 } from "../../typechain-types/contracts/vault/offchainAsset/OffchainAssetReceiptVault";
 import {
   deployOffChainAssetReceiptVault,
@@ -553,6 +554,29 @@ describe("OffChainAssetReceiptVault", async function () {
     )) as SnapshotEvent["args"];
 
     assert(id.eq(ethers.BigNumber.from(1)), `ID not set`);
+  });
+  it("SnapshotWithData event is emitted", async function () {
+    const signers = await ethers.getSigners();
+    const alice = signers[0];
+    const [vault] = await deployOffChainAssetReceiptVault();
+
+    await vault
+      .connect(alice)
+      .grantRole(await vault.connect(alice).ERC20SNAPSHOTTER(), alice.address);
+
+    const { sender, id, data } = (await getEventArgs(
+      await vault.connect(alice).snapshot([1]),
+      "SnapshotWithData",
+      vault
+    )) as SnapshotWithDataEvent["args"];
+
+    assert(id.eq(ethers.BigNumber.from(1)), `ID not set`);
+
+    assert(
+      sender === alice.address,
+      `Wrong sender. Expected ${alice.address}, got ${sender}`
+    );
+    assert(data === "0x01", `Wrong information. Expected 0x01, got ${data}`);
   });
   it("Sets correct erc20Tier and mintier", async function () {
     const [vault] = await deployOffChainAssetReceiptVault();
