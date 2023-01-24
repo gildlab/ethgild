95bd25b637c8e0ee125f96a8df944ed343ec187a
Nino Keldishvili <Ninokeldishvili@gmail.com>
2023-01-24 12:46:34 +0400

    Modify test to have one oracle

--- test/oracle/Oracle.test.ts
+++ test/oracle/Oracle.test.ts
@@ -22,81 +22,81 @@ describe("oracle", async function () {
     );
   });
   it("Checks price oracle does not change", async function () {
+    const now = await latestBlockNow();
+
     const quotePriceA = "194076000000";
-    const priceOracle = await getPriceOracle(8, quotePriceA);
-
-    const price = await priceOracle.price();
-
+    const xauDecimalsA = 8;
     const quotePriceB = "1940760000000000000000";
-    const priceOracleB = await getPriceOracle(18, quotePriceB);
+    const xauDecimalsB = 18;
 
-    const priceB = await priceOracleB.price();
+    const oracleFactory = await ethers.getContractFactory(
+      "MockChainlinkDataFeed"
+    );
+    const basePriceOracle =
+      (await oracleFactory.deploy()) as MockChainlinkDataFeed;
+    await basePriceOracle.deployed();
 
-    assert(price.eq(priceB), `Wrong price`);
+    await basePriceOracle.setDecimals(usdDecimals);
+    await basePriceOracle.setRoundData(1, {
+      startedAt: now,
+      updatedAt: now,
+      answer: basePrice,
+      answeredInRound: 1,
+    });
+
+    const quotePriceOracle =
+      (await oracleFactory.deploy()) as MockChainlinkDataFeed;
+    await quotePriceOracle.deployed();
+
+    await quotePriceOracle.setDecimals(xauDecimalsA);
+    await quotePriceOracle.setRoundData(1, {
+      startedAt: now,
+      updatedAt: now,
+      answer: quotePriceA,
+      answeredInRound: 1,
+    });
+
+    // 1 hour
+    const baseStaleAfter = 60 * 60;
+    // 48 hours
+    const quoteStaleAfter = 48 * 60 * 60;
+
+    const chainlinkFeedPriceOracleFactory = await ethers.getContractFactory(
+      "ChainlinkFeedPriceOracle"
+    );
+    const chainlinkFeedPriceOracleBase =
+      await chainlinkFeedPriceOracleFactory.deploy({
+        feed: basePriceOracle.address,
+        staleAfter: baseStaleAfter,
+      });
+    const chainlinkFeedPriceOracleQuote =
+      await chainlinkFeedPriceOracleFactory.deploy({
+        feed: quotePriceOracle.address,
+        staleAfter: quoteStaleAfter,
+      });
+    await chainlinkFeedPriceOracleBase.deployed();
+    await chainlinkFeedPriceOracleQuote.deployed();
+
+    const twoPriceOracleFactory = await ethers.getContractFactory(
+      "TwoPriceOracle"
+    );
+    const twoPriceOracle = (await twoPriceOracleFactory.deploy({
+      base: chainlinkFeedPriceOracleBase.address,
+      quote: chainlinkFeedPriceOracleQuote.address,
+    })) as TwoPriceOracle;
+
+    const priceA = await twoPriceOracle.price();
+
+    await quotePriceOracle.setDecimals(xauDecimalsB);
+    await quotePriceOracle.setRoundData(1, {
+      startedAt: now,
+      updatedAt: now,
+      answer: quotePriceB,
+      answeredInRound: 1,
+    });
+
+    const priceB = await twoPriceOracle.price();
+
+    assert(priceA.eq(priceB), `Wrong price`);
   });
 });
-
-const getPriceOracle = async (
-  xauDecimals,
-  quotePrice
-): Promise<TwoPriceOracle> => {
-  const now = await latestBlockNow();
-
-  const oracleFactory = await ethers.getContractFactory(
-    "MockChainlinkDataFeed"
-  );
-  const basePriceOracle =
-    (await oracleFactory.deploy()) as MockChainlinkDataFeed;
-  await basePriceOracle.deployed();
-
-  await basePriceOracle.setDecimals(usdDecimals);
-  await basePriceOracle.setRoundData(1, {
-    startedAt: now,
-    updatedAt: now,
-    answer: basePrice,
-    answeredInRound: 1,
-  });
-
-  const quotePriceOracle =
-    (await oracleFactory.deploy()) as MockChainlinkDataFeed;
-  await quotePriceOracle.deployed();
-
-  await quotePriceOracle.setDecimals(xauDecimals);
-  await quotePriceOracle.setRoundData(1, {
-    startedAt: now,
-    updatedAt: now,
-    answer: quotePrice,
-    answeredInRound: 1,
-  });
-
-  // 1 hour
-  const baseStaleAfter = 60 * 60;
-  // 48 hours
-  const quoteStaleAfter = 48 * 60 * 60;
-
-  const chainlinkFeedPriceOracleFactory = await ethers.getContractFactory(
-    "ChainlinkFeedPriceOracle"
-  );
-  const chainlinkFeedPriceOracleBase =
-    await chainlinkFeedPriceOracleFactory.deploy({
-      feed: basePriceOracle.address,
-      staleAfter: baseStaleAfter,
-    });
-  const chainlinkFeedPriceOracleQuote =
-    await chainlinkFeedPriceOracleFactory.deploy({
-      feed: quotePriceOracle.address,
-      staleAfter: quoteStaleAfter,
-    });
-  await chainlinkFeedPriceOracleBase.deployed();
-  await chainlinkFeedPriceOracleQuote.deployed();
-
-  const twoPriceOracleFactory = await ethers.getContractFactory(
-    "TwoPriceOracle"
-  );
-  const twoPriceOracle = (await twoPriceOracleFactory.deploy({
-    base: chainlinkFeedPriceOracleBase.address,
-    quote: chainlinkFeedPriceOracleQuote.address,
-  })) as TwoPriceOracle;
-
-  return twoPriceOracle;
-};
