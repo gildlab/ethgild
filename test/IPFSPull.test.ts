0cd9f700f309985db1e6f08ec0fa681dd593b3f5
--global <--global>
2023-02-14 16:33:21 +0400

    Test the URL on a deployed receipt contract instead of hardcoded one

--- test/IPFSPull.test.ts
+++ test/IPFSPull.test.ts
@@ -1,12 +1,41 @@
 import erc1155Metadata from "../erc1155Metadata/ReceiptMetadata.json";
 import assert from "assert";
+import { artifacts, ethers } from "hardhat";
+import { Receipt, ReceiptFactory } from "../typechain-types";
+import { expectedUri, getEventArgs } from "./util";
+import { Contract } from "ethers";
 
 describe("IPFS pull", async function () {
   it("Pulls data from ipfs and checks it", async function () {
     this.timeout(0);
-    const resp = await fetch(
-      "https://ipfs.io/ipfs/bafkreih7cvpjocgrk7mgdel2hvjpquc26j4jo2jkez5y2qdaojfil7vley"
+
+    const signers = await ethers.getSigners();
+    const alice = signers[0];
+
+    const receiptFactoryFactory = await ethers.getContractFactory(
+      "ReceiptFactory"
     );
+    const receiptFactoryContract =
+      (await receiptFactoryFactory.deploy()) as ReceiptFactory;
+    await receiptFactoryContract.deployed();
+
+    let tx = await receiptFactoryContract.createChild([]);
+
+    const { child } = await getEventArgs(
+      tx,
+      "NewChild",
+      receiptFactoryContract
+    );
+
+    let childContract = new Contract(
+      child,
+      (await artifacts.readArtifact("Receipt")).abi
+    ) as Receipt;
+
+    let uri = await childContract.connect(alice).uri(1);
+
+    const resp = await fetch(`https://ipfs.io/ipfs/${uri.slice(7)}`);
+
     const ipfsData = await resp.json().catch(console.error);
 
     assert(
