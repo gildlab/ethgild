33f21b402624e67224c6ec8ab98c133fbd56d830
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-07 17:13:52 +0400

    Add npm install

--- flake.nix
+++ flake.nix
@@ -86,13 +86,7 @@
             packages.ipfs-add
             packages.security-check
           ];
-          shellHook = ''
-              touch .env && source .env
-              export PATH=$(npm bin):$PATH
-              # keep it fresh
-              npm install
-              ${rainix.devShells.${system}.default.shellHook}
-            '';
+          shellHook = rainix.devShells.${system}.default.shellHook;
           buildInputs = rainix.devShells.${system}.default.buildInputs;
           nativeBuildInputs = rainix.devShells.${system}.default.nativeBuildInputs;
         };
