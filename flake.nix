105d8f9e12195f02991a0ac5c8ddbad013b1f141
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-09 13:20:53 +0400

    Remove npm install and quotes from hardhat test

--- flake.nix
+++ flake.nix
@@ -13,43 +13,42 @@
         pkgs = rainix.pkgs.${system};
         rust-toolchain = rainix.rust-toolchain.${system};
 
-          ci-lint = rainix.mkTask.${system} {
-            name = "ci-lint";
-            body = ''
-              npm install
-              solhint 'contracts/**/*.sol'
-              prettier --check .
-            '';
-          };
-          flush-all = rainix.mkTask.${system} {
-            name = "flush-all";
-            body = ''
-              rm -rf artifacts
-              rm -rf cache
-              rm -rf node_modules
-              rm -rf typechain
-              rm -rf typechain-types
-              rm -rf bin
-            '';
-          };
-          security-check = rainix.mkTask.${system} {
-            name = "security-check";
-            body = ''
-              flush-all
-              npm install
+        ci-lint = rainix.mkTask.${system} {
+          name = "ci-lint";
+          body = ''
+            solhint 'contracts/**/*.sol'
+            prettier --check .
+          '';
+        };
+        flush-all = rainix.mkTask.${system} {
+          name = "flush-all";
+          body = ''
+            rm -rf artifacts
+            rm -rf cache
+            rm -rf node_modules
+            rm -rf typechain
+            rm -rf typechain-types
+            rm -rf bin
+          '';
+        };
+        security-check = rainix.mkTask.${system} {
+          name = "security-check";
+          body = ''
+            flush-all
+            npm install
 
-              # Run slither against all our contracts.
-              # Disable npx as nix-shell already handles availability of what we need.
-              # Dependencies and tests are out of scope.
-              slither . --npx-disable --filter-paths="contracts/test" --exclude-dependencies --fail-high
-            '';
-          };
-          ipfs-add = rainix.mkTask.${system} {
-            name = "ipfs-add";
-            body = ''
-              ipfs add -r --pin --cid-version 1 erc1155Metadata
-            '';
-          };
+            # Run slither against all our contracts.
+            # Disable npx as nix-shell already handles availability of what we need.
+            # Dependencies and tests are out of scope.
+            slither . --npx-disable --filter-paths="contracts/test" --exclude-dependencies --fail-high
+          '';
+        };
+        ipfs-add = rainix.mkTask.${system} {
+          name = "ipfs-add";
+          body = ''
+            ipfs add -r --pin --cid-version 1 erc1155Metadata
+          '';
+        };
       in {
         devShells.default = pkgs.mkShell {
           shellHook = rainix.devShells.${system}.default.shellHook;
