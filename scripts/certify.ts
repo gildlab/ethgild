e473c87092688bdc0e61f3b88e68c79cf05e3732
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-21 17:13:12 +0400

    Deploy new contract in script. add script in package.json

--- scripts/certify.ts
+++ scripts/certify.ts
@@ -1,27 +1,50 @@
 import { artifacts, ethers } from "hardhat";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
+import { getEventArgs } from "../test/util";
+import { Contract } from "ethers";
+import type { OffchainAssetReceiptVault } from "../typechain-types";
 
 dotenv.config();
 
 async function main() {
   const { PRIVATE_KEY, ARBITRUM_SEPOLIA_RPC_URL } = process.env;
-  // const { PRIVATE_KEY, ETHEREUM_SEPOLIA_URL } = process.env;
 
-  if (!PRIVATE_KEY || !ARBITRUM_SEPOLIA_RPC_URL) {
-  // if (!PRIVATE_KEY || !ETHEREUM_SEPOLIA_URL) {
+  if ( !PRIVATE_KEY || !ARBITRUM_SEPOLIA_RPC_URL ) {
     throw new Error("Please set PRIVATE_KEY and ARBITRUM_RPC_URL in your .env file");
   }
 
   const provider = new ethers.providers.JsonRpcProvider(ARBITRUM_SEPOLIA_RPC_URL);
-  // const provider = new ethers.providers.JsonRpcProvider(ETHEREUM_SEPOLIA_URL);
-  // const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
   const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
-  const contractAbi = (await artifacts.readArtifact("OffchainAssetReceiptVault")).abi
-  const contractAddress = '0xfc34d331cc7a4b461b7ded2f6835a837411d8684'; // arbitrum sepolia
-  // const contractAddress = '0xAFD4239F28297F61AB8f6bE03CeD343E2389739F'; //ethereum-sepolia
+  const contractFactoryAbi = ( await artifacts.readArtifact("OffchainAssetReceiptVaultFactory") ).abi;
+  //factory contract address on arbitrum sepolia
+  const contractFactoryAddress = "0xf1A14e96977E8dE295Ba9612691D127B157d1371";
 
-  const contract = new ethers.Contract(contractAddress, contractAbi, wallet);
+  const contractFactory = new ethers.Contract(contractFactoryAddress, contractFactoryAbi, wallet);
+
+  const constructionConfig = {
+    admin: wallet.address,
+    vaultConfig: {
+      asset: ethers.constants.AddressZero,
+      name: `OffchainAssetVaul-${ new Date().getTime() }`,
+      symbol: "OAV"
+    }
+  };
+
+  let tx = await contractFactory.connect(wallet).createChildTyped(
+    constructionConfig
+  );
+
+  const { child } = await getEventArgs(
+    tx,
+    "NewChild",
+    contractFactory
+  );
+
+  let contract = new Contract(
+    child,
+    ( await artifacts.readArtifact("OffchainAssetReceiptVault") ).abi
+  ) as OffchainAssetReceiptVault;
 
   try {
     const blockNum = await ethers.provider.getBlockNumber();
@@ -29,17 +52,17 @@ async function main() {
     const _certifiedUntil = block.timestamp + 100;
 
     //Grant role
-    // contract.grantRole(await contract.CERTIFIER(), alice.address);
+    await contract.connect(wallet).grantRole(await contract.connect(wallet).CERTIFIER(), wallet.address);
 
-    // Replace with the function you want to test and its parameters
-    const tx = await contract.certify(_certifiedUntil, blockNum, false, []);
-    console.log('Transaction sent:', tx.hash);
+    //Call certify function
+    const tx = await contract.connect(wallet).certify(_certifiedUntil, blockNum, false, []);
+    console.log("Transaction sent:", tx.hash);
 
     // Wait for the transaction to be mined
     const receipt = await tx.wait();
-    console.log('Transaction mined:', receipt);
-  } catch (error) {
-    console.error('Error occurred:', error);
+    console.log("Transaction mined:", receipt);
+  } catch ( error ) {
+    console.error("Error occurred:", error);
   }
 }
 
