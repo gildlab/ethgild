46d8a11011ec1774651619981ecf7c1c3d6c3400
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-22 10:42:00 +0400

    Test for construction OffchainAssetReceiptVaultFactory

---
+++ test/foundry/OffchainAssetReceiptVaultFactory.t.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.8.17;
+
+import "../../contracts/vault/receipt/ReceiptVault.sol";
+import "../../contracts/vault/receipt/ReceiptFactory.sol";
+
+import "forge-std/Test.sol";
+
+import "../../contracts/vault/offchainAsset/OffchainAssetReceiptVaultFactory.sol";
+import "../../contracts/vault/offchainAsset/OffchainAssetReceiptVault.sol";
+
+contract OffChainAssetReceiptVaultTest is Test {
+    OffchainAssetReceiptVaultFactory factory;
+    OffchainAssetVaultConfig offchainAssetVaultConfig;
+    VaultConfig vaultConfig;
+    OffchainAssetReceiptVault implementation;
+    ReceiptFactory receiptFactory;
+    ReceiptVaultFactoryConfig factoryConfig;
+    address alice;
+
+    function setUp() public {
+        implementation = new OffchainAssetReceiptVault();
+        receiptFactory = new ReceiptFactory();
+
+        // Set up factory config
+        factoryConfig = ReceiptVaultFactoryConfig({
+            implementation: address(implementation),
+            receiptFactory: address(receiptFactory)
+        });
+
+        // Create OffchainAssetReceiptVaultFactory contract
+        factory = new OffchainAssetReceiptVaultFactory(factoryConfig);
+
+    }
+
+    function testOffchainAssetReceiptVaultFactoryConstuction() public view{
+        assert(address(factory) != address(0));
+    }
+}
