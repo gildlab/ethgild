88d6d3465ae7ae013951f3ed18d63d15c7a4a735
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-22 11:09:49 +0400

    Test to check child creation

--- test/foundry/OffchainAssetReceiptVaultFactory.t.sol
+++ test/foundry/OffchainAssetReceiptVaultFactory.t.sol
@@ -4,6 +4,7 @@ import "../../contracts/vault/receipt/ReceiptVault.sol";
 import "../../contracts/vault/receipt/ReceiptFactory.sol";
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 
 import "../../contracts/vault/offchainAsset/OffchainAssetReceiptVaultFactory.sol";
 import "../../contracts/vault/offchainAsset/OffchainAssetReceiptVault.sol";
@@ -15,6 +16,7 @@ contract OffChainAssetReceiptVaultTest is Test {
     OffchainAssetReceiptVault implementation;
     ReceiptFactory receiptFactory;
     ReceiptVaultFactoryConfig factoryConfig;
+    OffchainAssetReceiptVault vault;
     address alice;
 
     function setUp() public {
@@ -34,5 +36,30 @@ contract OffChainAssetReceiptVaultTest is Test {
 
     function testOffchainAssetReceiptVaultFactoryConstuction() public view{
         assert(address(factory) != address(0));
+    }
+
+    function testCreateChild() public{
+        // Get the first signer address
+        alice = vm.addr(1);
+
+        string memory assetName = "Asset Name";
+        string memory assetSymbol = "ASSET";
+
+        // VaultConfig to create child contract
+        vaultConfig = VaultConfig(
+            address(0),
+            assetName,
+            assetSymbol
+        );
+
+        offchainAssetVaultConfig = OffchainAssetVaultConfig({
+            admin: alice,
+            vaultConfig: vaultConfig
+        });
+
+        vault = factory.createChildTyped(offchainAssetVaultConfig);
+        assert(address(vault) != address(0));
+        assert(keccak256(abi.encodePacked(vault.name())) == keccak256(abi.encodePacked(assetName)));
+        assert(keccak256(abi.encodePacked(vault.symbol())) == keccak256(abi.encodePacked(assetSymbol)));
     }
 }
