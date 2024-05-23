9a41399cd9984ca4a5b239d5f291782109f65b25
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-23 17:29:12 +0400

    Test testNonZeroAsset and some refactor and fmt

--- test/foundry/OffchainAssetReceiptVault.t.sol
+++ test/foundry/OffchainAssetReceiptVault.t.sol
@@ -4,7 +4,6 @@ import "../../contracts/vault/receipt/ReceiptVault.sol";
 import "../../contracts/vault/receipt/ReceiptFactory.sol";
 
 import "forge-std/Test.sol";
-import "forge-std/console.sol";
 
 import "../../contracts/vault/offchainAsset/OffchainAssetReceiptVaultFactory.sol";
 import "../../contracts/vault/offchainAsset/OffchainAssetReceiptVault.sol";
@@ -31,37 +30,31 @@ contract OffChainAssetReceiptVaultTest is Test {
 
         // Create OffchainAssetReceiptVaultFactory contract
         factory = new OffchainAssetReceiptVaultFactory(factoryConfig);
+
         // Get the first signer address
         alice = vm.addr(1);
+    }
 
+    function testZeroAdmin() public {
         string memory assetName = "Asset Name";
         string memory assetSymbol = "ASSET";
 
-        // VaultConfig to create child contract
         vaultConfig = VaultConfig(address(0), assetName, assetSymbol);
+        offchainAssetVaultConfig = OffchainAssetVaultConfig({admin: address(0), vaultConfig: vaultConfig});
 
-
-    }
-
-    function testOffchainAssetReceiptVaultZeroAdmin() public {
-        offchainAssetVaultConfig = OffchainAssetVaultConfig({admin: address (0), vaultConfig: vaultConfig});
         vm.expectRevert(abi.encodeWithSignature("ZeroAdmin()"));
         vault = factory.createChildTyped(offchainAssetVaultConfig);
     }
 
-//    function testDepositWithoutDepositorRole() public {
-//        // Prank as Alice for the transaction
-//        vm.startPrank(alice);
-//
-//        // Get the second signer address
-//        address bob = vm.addr(2);
-//        uint256 aliceAssets = 10;
-//        bytes memory receiptInformation = "";
-//
-//        vm.expectRevert(abi.encodeWithSignature("MinShareRatio(uint256,uint256)", shareRatio, 0));
-//        vault.deposit(aliceAssets, bob, shareRatio, receiptInformation);
-//        vm.stopPrank();
-//    }
+    function testNonZeroAsset() public {
+        address asset = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+        string memory assetName = "Asset Name";
+        string memory assetSymbol = "ASSET";
 
+        vaultConfig = VaultConfig(asset, assetName, assetSymbol);
+        offchainAssetVaultConfig = OffchainAssetVaultConfig({admin: address(0), vaultConfig: vaultConfig});
 
+        vm.expectRevert(abi.encodeWithSignature("NonZeroAsset()"));
+        vault = factory.createChildTyped(offchainAssetVaultConfig);
+    }
 }
