cc59e194b0c6c4f44c89b3580797b5da09208cc4
ninokeldishvili <ninokeldishvili@gmail.com>
2024-05-28 11:10:03 +0400

    Fuzz alice address

--- test/foundry/OffchainAssetReceiptVaultFactory.t.sol
+++ test/foundry/OffchainAssetReceiptVaultFactory.t.sol
@@ -10,6 +10,8 @@ import {
     OffchainAssetReceiptVaultConfig
 } from "../../contracts/vault/offchainAsset/OffchainAssetReceiptVault.sol";
 
+uint256 constant MAX_VALID_PRIVATE_KEY = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141;
+
 contract OffChainAssetReceiptVaultFactoryTest is Test, CreateOffchainAssetReceiptVaultFactory {
     OffchainAssetVaultConfig offchainAssetVaultConfig;
     VaultConfig vaultConfig;
@@ -31,9 +33,9 @@ contract OffChainAssetReceiptVaultFactoryTest is Test, CreateOffchainAssetReceip
 
     ///Test OffchainAssetReceiptVaultFactory child is created with correct properties
     ///and that OffchainAssetReceiptVaultInitialized event is emitted
-    function testCreateChild(address alice, string memory assetName, string memory assetSymbol) external {
-        // Exclude the zero address
-        vm.assume(alice != address(0));
+    function testCreateChild(uint256 fuzzedKeyAlice, string memory assetName, string memory assetSymbol) external {
+        vm.assume(fuzzedKeyAlice > 0 && fuzzedKeyAlice < MAX_VALID_PRIVATE_KEY);
+        address alice = vm.addr(fuzzedKeyAlice);
 
         // VaultConfig to create child contract
         vaultConfig = VaultConfig(address(0), assetName, assetSymbol);
