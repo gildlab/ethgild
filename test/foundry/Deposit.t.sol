1c4d7c78e8c54406ed40bf9278e97ecb4ce08347
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-03 12:06:21 +0400

    Add test MinShareRatio

--- test/foundry/Deposit.t.sol
+++ test/foundry/Deposit.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CAL
 pragma solidity =0.8.17;
 
-import {VaultConfig} from "../../contracts/vault/receipt/ReceiptVault.sol";
+import {VaultConfig, MinShareRatio} from "../../contracts/vault/receipt/ReceiptVault.sol";
 import {CreateOffchainAssetReceiptVaultFactory} from "../../contracts/test/CreateOffchainAssetReceiptVaultFactory.sol";
 import {Test, Vm} from "forge-std/Test.sol";
 import "forge-std/console.sol";
@@ -156,6 +156,44 @@ contract DepositTest is Test, CreateOffchainAssetReceiptVaultFactory {
         vault.deposit(aliceAssets, bob, shareRatio, receiptInformation);
 
         assertEqUint(vault.totalSupply(), vault.totalAssets());
+
+        vm.stopPrank();
+    }
+
+    function testMinShareRatio(
+        uint256 aliceAssets,
+        bytes memory receiptInformation,
+        uint256 certifyUntil,
+        bytes memory data
+    ) external {
+        // Assume that aliceAssets is less than TOTAL_SUPPLY
+        aliceAssets = bound(aliceAssets, 1, TOTAL_SUPPLY - 1);
+        // Assume that certifyUntil is not zero and is in future
+        certifyUntil = bound(certifyUntil, 1, block.number + 1);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        //New testErc20 contract
+        TestErc20 testErc20Contract = new TestErc20();
+        testErc20Contract.transfer(alice, aliceAssets);
+        testErc20Contract.increaseAllowance(address(vault), aliceAssets);
+
+        // Grant CERTIFIER role to Alice
+        vault.grantRole(vault.CERTIFIER(), alice);
+
+        // Get the current block number
+        uint256 blockNum = block.number;
+
+        // Set up expected parameters
+        bool forceUntil = false;
+
+        // Call the certify function
+        vault.certify(certifyUntil, blockNum, forceUntil, data);
+
+        vault.grantRole(vault.DEPOSITOR(), alice);
+        vm.expectRevert(abi.encodeWithSelector(MinShareRatio.selector, shareRatio + 1, shareRatio));
+        vault.deposit(aliceAssets, alice, shareRatio + 1, receiptInformation);
 
         vm.stopPrank();
     }
