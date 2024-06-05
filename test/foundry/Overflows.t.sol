0eebc4f7bff9e4424bdca154783f16724a379441
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-05 18:52:45 +0400

    Check positive overflow

---
+++ test/foundry/Overflows.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.17;
+
+import {CreateOffchainAssetReceiptVaultFactory} from "../../contracts/test/CreateOffchainAssetReceiptVaultFactory.sol";
+import {Test, Vm, stdError} from "forge-std/Test.sol";
+import {OffchainAssetReceiptVault} from "../../contracts/vault/offchainAsset/OffchainAssetReceiptVault.sol";
+import {OffchainAssetVaultCreator} from "./OffchainAssetVaultCreator.sol";
+
+contract Overflows is Test, CreateOffchainAssetReceiptVaultFactory {
+    /// Check positive overflow
+    function testNegativeOverflow(
+        uint256 fuzzedKeyAlice,
+        bytes memory fuzzedReceiptInformation,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        address alice = vm.addr(fuzzedKeyAlice);
+
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        vault.grantRole(vault.DEPOSITOR(), alice);
+
+        vm.expectRevert(stdError.arithmeticError);
+        vault.mint(UINT256_MAX + 1, alice, 1e18, fuzzedReceiptInformation);
+
+        vm.stopPrank();
+    }
+}
