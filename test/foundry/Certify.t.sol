bc0a7a2900822ebbba4ee6cddfb4d2d342428fb4
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-06 21:51:48 +0400

    Test certify with force until true

--- test/foundry/Certify.t.sol
+++ test/foundry/Certify.t.sol
@@ -15,6 +15,7 @@ import {OffchainAssetVaultCreator} from "./OffchainAssetVaultCreator.sol";
 contract CertifyTest is Test, CreateOffchainAssetReceiptVaultFactory {
     event Certify(address sender, uint256 certifyUntil, uint256 referenceBlockNumber, bool forceUntil, bytes data);
 
+    /// Test certify event
     function testCertify(
         uint256 fuzzedKeyAlice,
         string memory assetName,
@@ -47,6 +48,7 @@ contract CertifyTest is Test, CreateOffchainAssetReceiptVaultFactory {
         vm.stopPrank();
     }
 
+    /// Test certify reverts on zero certify until
     function testCertifyRevertOnZeroCertifyUntil(
         uint256 fuzzedKeyAlice,
         string memory assetName,
@@ -75,6 +77,7 @@ contract CertifyTest is Test, CreateOffchainAssetReceiptVaultFactory {
         vm.stopPrank();
     }
 
+    /// Test certify reverts on future reference
     function testCertifyRevertOnFutureReferenceBlock(
         uint256 fuzzedKeyAlice,
         string memory assetName,
@@ -105,6 +108,48 @@ contract CertifyTest is Test, CreateOffchainAssetReceiptVaultFactory {
 
         // Call the certify function
         vault.certify(certifyUntil, fuzzedFutureBlockNumber, false, data);
+
+        vm.stopPrank();
+    }
+
+    /// Test certify with force until true
+    function testCertifyWithForceUntilTrue(
+        uint256 fuzzedKeyAlice,
+        string memory assetName,
+        string memory assetSymbol,
+        uint256 certifyUntil,
+        uint256 certifyUntilPast,
+        uint256 fuzzedBlockNumber,
+        bytes memory data
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        address alice = vm.addr(fuzzedKeyAlice);
+
+        fuzzedBlockNumber = bound(fuzzedBlockNumber, 1, 1e6);
+        certifyUntil = bound(certifyUntil, 1, fuzzedBlockNumber);
+        vm.assume(certifyUntil > certifyUntilPast && certifyUntilPast != 0);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+
+        // Grant CERTIFIER role to Alice
+        vault.grantRole(vault.CERTIFIER(), alice);
+
+        // Expect the Certify event
+        vm.expectEmit(true, true, true, true);
+        emit Certify(alice, certifyUntil, block.number, false, data);
+
+        // Call the certify function
+        vault.certify(certifyUntil, block.number, false, data);
+
+        // Expect the Certify event
+        vm.expectEmit(true, true, true, true);
+        emit Certify(alice, certifyUntilPast, block.number, true, data);
+
+        // Call the certify function
+        vault.certify(certifyUntilPast, block.number, true, data);
 
         vm.stopPrank();
     }
