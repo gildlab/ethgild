332849cb3491e201594046df63a86e9d96b3b8ff
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-07 16:48:19 +0400

    Test withdraw reverts when withdrawing more than balance

--- test/foundry/Withdraw.t.sol
+++ test/foundry/Withdraw.t.sol
@@ -66,4 +66,41 @@ contract WithdrawTest is Test, CreateOffchainAssetReceiptVaultFactory {
         // Stop the prank
         vm.stopPrank();
     }
+
+    /// Test withdraw reverts when withdrawing more than balance
+    function testWithdrawMoreThanBalance(
+        uint256 fuzzedKeyAlice,
+        uint256 aliceAssets,
+        uint256 assetsToWithdraw,
+        uint256 shareRatio,
+        bytes memory data,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        shareRatio = bound(shareRatio, 1, 1e18);
+        address alice = vm.addr(fuzzedKeyAlice);
+        // Assume that aliceAssets is not 0
+        vm.assume(aliceAssets != 0);
+        vm.assume(assetsToWithdraw > aliceAssets);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+        vault.grantRole(vault.DEPOSITOR(), alice);
+        vault.grantRole(vault.WITHDRAWER(), alice);
+
+        // Call the deposit function
+        vault.deposit(aliceAssets, alice, shareRatio, data);
+
+        // withdraw should revert: burn amount exceeds balance
+        vm.expectRevert();
+        // Call withdraw function
+        vault.withdraw(assetsToWithdraw, alice, alice, 1, data);
+
+        // Stop the prank
+        vm.stopPrank();
+    }
 }
