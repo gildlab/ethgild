a3dbd9e0550febe6d88c2cf8fc4eb157c73fc060
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-07 17:31:49 +0400

    Test withdraw on someone else

--- test/foundry/Withdraw.t.sol
+++ test/foundry/Withdraw.t.sol
@@ -331,4 +331,51 @@ contract WithdrawTest is Test, CreateOffchainAssetReceiptVaultFactory {
         // Stop the prank
         vm.stopPrank();
     }
+
+    /// Test withdraw on someone else
+    function testWithdrawOnfSomeoneElse(
+        uint256 fuzzedKeyAlice,
+        uint256 fuzzedKeyBob,
+        uint256 aliceAssets,
+        uint256 shareRatio,
+        bytes memory data,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        address alice = vm.addr(fuzzedKeyAlice);
+
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyBob = bound(fuzzedKeyBob, 1, SECP256K1_ORDER - 1);
+        address bob = vm.addr(fuzzedKeyBob);
+
+        vm.assume(alice != bob);
+
+        shareRatio = bound(shareRatio, 1, 1e18);
+        // Assume that aliceAssets is not 0
+        vm.assume(aliceAssets != 0);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+        vault.grantRole(vault.DEPOSITOR(), alice);
+        vault.grantRole(vault.WITHDRAWER(), alice);
+
+        // Call the deposit function
+        vault.deposit(aliceAssets, alice, shareRatio, data);
+
+        // Calculate expected shares
+        uint256 expectedShares = aliceAssets.fixedPointMul(1e18, Math.Rounding.Up);
+        // Set up the event expectation for DepositWithReceipt
+        vm.expectEmit(true, true, true, true);
+        emit WithdrawWithReceipt(alice, bob, alice, aliceAssets, expectedShares, 1, data);
+
+        // Call withdraw function
+        vault.withdraw(aliceAssets, bob, alice, 1, data);
+
+        // Stop the prank
+        vm.stopPrank();
+    }
 }
--- test/offchainAsset/OffChainAssetReceiptVault.test.ts
+++ test/offchainAsset/OffChainAssetReceiptVault.test.ts
@@ -1795,69 +1795,6 @@ describe("OffChainAssetReceiptVault", async function () {
       `wrong receipt information sender ${alice.address} got ${sender}`
     );
   });
-  it("Withdraw on someone else", async function () {
-    const [vault, receipt] = await deployOffChainAssetReceiptVault();
-
-    const signers = await ethers.getSigners();
-    const alice = signers[0];
-    const bob = signers[1];
-    const id = 1;
-
-    //grant depositor role to alice
-    await vault
-      .connect(alice)
-      .grantRole(await vault.connect(alice).DEPOSITOR(), alice.address);
-
-    const testErc20 = await ethers.getContractFactory("TestErc20");
-    const testErc20Contract = (await testErc20.deploy()) as TestErc20;
-    await testErc20Contract.deployed();
-
-    const assets = ethers.BigNumber.from(30);
-    await testErc20Contract.transfer(alice.address, assets);
-    await testErc20Contract
-      .connect(alice)
-      .increaseAllowance(vault.address, assets);
-
-    const shares = ethers.BigNumber.from(10);
-    await vault
-      .connect(alice)
-      ["mint(uint256,address,uint256,bytes)"](shares, alice.address, id, []);
-
-    const balance = await receipt.connect(alice).balanceOf(alice.address, id);
-
-    await vault
-      .connect(alice)
-      .grantRole(await vault.connect(alice).WITHDRAWER(), alice.address);
-
-    let bobBalanceVault = await vault.connect(alice).balanceOf(bob.address);
-    let aliceBalanceVault = await vault.connect(alice).balanceOf(alice.address);
-
-    await vault
-      .connect(alice)
-      ["withdraw(uint256,address,address,uint256,bytes)"](
-        balance,
-        bob.address,
-        alice.address,
-        id,
-        []
-      );
-
-    let bobBalanceVaultAft = await vault.connect(alice).balanceOf(bob.address);
-    let aliceBalanceVaultAft = await vault
-      .connect(alice)
-      .balanceOf(alice.address);
-
-    assert(
-      bobBalanceVaultAft.eq(bobBalanceVault),
-      `Wrong shares for bob ${bobBalanceVaultAft} got ${bobBalanceVaultAft}`
-    );
-    assert(
-      aliceBalanceVaultAft.eq(aliceBalanceVault.sub(balance)),
-      `Wrong shares for alice ${aliceBalanceVault.sub(
-        balance
-      )} got ${aliceBalanceVaultAft}`
-    );
-  });
   it("Redeems on someone else", async function () {
     const [vault, receipt] = await deployOffChainAssetReceiptVault();
 
