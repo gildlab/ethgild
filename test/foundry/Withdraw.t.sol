adcd586f1a81caf2c6a537bc1efb2c5a12651d94
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-07 16:57:53 +0400

    Test withdraw function reverts without WITHDRAWER role

--- test/foundry/Withdraw.t.sol
+++ test/foundry/Withdraw.t.sol
@@ -28,6 +28,39 @@ contract WithdrawTest is Test, CreateOffchainAssetReceiptVaultFactory {
         bytes receiptInformation
     );
 
+    /// Test withdraw function reverts without WITHDRAWER role
+    function testWithdrawRevertsWithoutRole(
+        uint256 fuzzedKeyAlice,
+        uint256 aliceAssets,
+        uint256 shareRatio,
+        bytes memory data,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        shareRatio = bound(shareRatio, 1, 1e18);
+        address alice = vm.addr(fuzzedKeyAlice);
+        // Assume that aliceAssets is not 0
+        vm.assume(aliceAssets != 0);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+        vault.grantRole(vault.DEPOSITOR(), alice);
+
+        // Call the deposit function
+        vault.deposit(aliceAssets, alice, shareRatio, data);
+
+        vm.expectRevert();
+        // Call withdraw function
+        vault.withdraw(aliceAssets, alice, alice, 1, data);
+
+        // Stop the prank
+        vm.stopPrank();
+    }
+
     /// Test withdraw function emits WithdrawWithReceipt event
     function testWithdraw(
         uint256 fuzzedKeyAlice,
