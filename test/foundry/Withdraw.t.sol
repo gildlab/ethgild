4dd80f1dd9a2c0b3eb2e51eed81c096fc863748c
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-07 17:15:15 +0400

    Test withdraw reverts on errors ZeroAssetsAmount, ZeroSharesAmount, ZeroReceiver, InvalidId, ZeroOwner

--- test/foundry/Withdraw.t.sol
+++ test/foundry/Withdraw.t.sol
@@ -10,6 +10,7 @@ import {
 } from "../../contracts/vault/receipt/ReceiptVault.sol";
 import {CreateOffchainAssetReceiptVaultFactory} from "../../contracts/test/CreateOffchainAssetReceiptVaultFactory.sol";
 import {Test, Vm} from "forge-std/Test.sol";
+import "forge-std/console.sol";
 import {OffchainAssetReceiptVault} from "../../contracts/vault/offchainAsset/OffchainAssetReceiptVault.sol";
 import {LibFixedPointMath, Math} from "@rainprotocol/rain-protocol/contracts/math/LibFixedPointMath.sol";
 import {OffchainAssetVaultCreator} from "./OffchainAssetVaultCreator.sol";
@@ -128,10 +129,151 @@ contract WithdrawTest is Test, CreateOffchainAssetReceiptVaultFactory {
         // Call the deposit function
         vault.deposit(aliceAssets, alice, shareRatio, data);
 
-        // withdraw should revert: burn amount exceeds balance
+        // withdraw should revert
         vm.expectRevert();
         // Call withdraw function
         vault.withdraw(assetsToWithdraw, alice, alice, 1, data);
+
+        // Stop the prank
+        vm.stopPrank();
+    }
+
+    /// Test withdraw reverts on ZeroAssetsAmount
+    function testWithdrawZeroAssetsAmount(
+        uint256 fuzzedKeyAlice,
+        uint256 aliceAssets,
+        uint256 shareRatio,
+        bytes memory data,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        shareRatio = bound(shareRatio, 1, 1e18);
+        address alice = vm.addr(fuzzedKeyAlice);
+        // Assume that aliceAssets is not 0
+        vm.assume(aliceAssets != 0);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+        vault.grantRole(vault.DEPOSITOR(), alice);
+        vault.grantRole(vault.WITHDRAWER(), alice);
+
+        // Call the deposit function
+        vault.deposit(aliceAssets, alice, shareRatio, data);
+
+        // withdraw should revert
+        vm.expectRevert(abi.encodeWithSelector(ZeroAssetsAmount.selector));
+        // Call withdraw function
+        vault.withdraw(0, alice, alice, 1, data);
+
+        // Stop the prank
+        vm.stopPrank();
+    }
+
+    /// Test withdraw reverts on ZeroReceiver
+    function testWithdrawZeroReceiver(
+        uint256 fuzzedKeyAlice,
+        uint256 aliceAssets,
+        uint256 shareRatio,
+        bytes memory data,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        shareRatio = bound(shareRatio, 1, 1e18);
+        address alice = vm.addr(fuzzedKeyAlice);
+        // Assume that aliceAssets is not 0
+        vm.assume(aliceAssets != 0);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+        vault.grantRole(vault.DEPOSITOR(), alice);
+        vault.grantRole(vault.WITHDRAWER(), alice);
+
+        // Call the deposit function
+        vault.deposit(aliceAssets, alice, shareRatio, data);
+
+        // withdraw should revert
+        vm.expectRevert(abi.encodeWithSelector(ZeroReceiver.selector));
+        // Call withdraw function
+        vault.withdraw(aliceAssets, address(0), alice, 1, data);
+
+        // Stop the prank
+        vm.stopPrank();
+    }
+
+    /// Test withdraw reverts on ZeroOwner
+    function testWithdrawZeroOwner(
+        uint256 fuzzedKeyAlice,
+        uint256 aliceAssets,
+        uint256 shareRatio,
+        bytes memory data,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        shareRatio = bound(shareRatio, 1, 1e18);
+        address alice = vm.addr(fuzzedKeyAlice);
+        // Assume that aliceAssets is not 0
+        vm.assume(aliceAssets != 0);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+        vault.grantRole(vault.DEPOSITOR(), alice);
+        vault.grantRole(vault.WITHDRAWER(), alice);
+
+        // Call the deposit function
+        vault.deposit(aliceAssets, alice, shareRatio, data);
+
+        // withdraw should revert
+        vm.expectRevert();
+        // Call withdraw function
+        vault.withdraw(aliceAssets, alice, address(0), 1, data);
+
+        // Stop the prank
+        vm.stopPrank();
+    }
+
+    /// Test withdraw reverts on InvalidId when id is 0
+    function testWithdrawInvalidId(
+        uint256 fuzzedKeyAlice,
+        uint256 aliceAssets,
+        uint256 shareRatio,
+        bytes memory data,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        shareRatio = bound(shareRatio, 1, 1e18);
+        address alice = vm.addr(fuzzedKeyAlice);
+        // Assume that aliceAssets is not 0
+        vm.assume(aliceAssets != 0);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+        vault.grantRole(vault.DEPOSITOR(), alice);
+        vault.grantRole(vault.WITHDRAWER(), alice);
+
+        // Call the deposit function
+        vault.deposit(aliceAssets, alice, shareRatio, data);
+
+        // withdraw should revert
+        vm.expectRevert(abi.encodeWithSelector(InvalidId.selector, 0));
+
+        // Call withdraw function
+        vault.withdraw(aliceAssets, alice, alice, 0, data);
 
         // Stop the prank
         vm.stopPrank();
