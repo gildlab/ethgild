58d45028c6f326af43b3f2c4e08b82830119d840
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-10 00:54:43 +0400

    Test to checks id increases on multiple Snapshot and check snapshot id

--- test/foundry/Snapshot.t.sol
+++ test/foundry/Snapshot.t.sol
@@ -22,13 +22,59 @@ contract SnapshotTest is Test, CreateOffchainAssetReceiptVaultFactory {
         OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
         vault.grantRole(vault.ERC20SNAPSHOTTER(), alice);
 
+        // Expect the Snapshot and SnapshotWithData events
         vm.expectEmit(true, true, true, true);
         emit Snapshot(1);
 
         vm.expectEmit(true, true, true, true);
         emit SnapshotWithData(alice, 1, data);
 
-        vault.snapshot(data);
+        uint256 snapshotId = vault.snapshot(data);
+
+        // Ensure the snapshotId is as expected
+        assertEq(snapshotId, 1);
+
+        vm.stopPrank();
+    }
+
+    /// Test to checks id increases on multiple Snapshot
+    function testMultipleSnapshot(
+        uint256 fuzzedKeyAlice,
+        bytes memory data,
+        string memory assetName,
+        string memory assetSymbol
+    ) external {
+        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
+        address alice = vm.addr(fuzzedKeyAlice);
+
+        // Prank as Alice for the transaction
+        vm.startPrank(alice);
+        OffchainAssetReceiptVault vault = OffchainAssetVaultCreator.createVault(factory, alice, assetName, assetSymbol);
+        vault.grantRole(vault.ERC20SNAPSHOTTER(), alice);
+
+        // Expect the Snapshot and SnapshotWithData events
+        vm.expectEmit(true, true, true, true);
+        emit Snapshot(1);
+
+        vm.expectEmit(true, true, true, true);
+        emit SnapshotWithData(alice, 1, data);
+
+        uint256 snapshotId = vault.snapshot(data);
+
+        // Ensure the snapshotId is as expected
+        assertEq(snapshotId, 1);
+
+        // Expect the Snapshot and SnapshotWithData events
+        vm.expectEmit(true, true, true, true);
+        emit Snapshot(2);
+
+        vm.expectEmit(true, true, true, true);
+        emit SnapshotWithData(alice, 2, data);
+
+        snapshotId = vault.snapshot(data);
+
+        // Ensure the snapshotId is as expected
+        assertEq(snapshotId, 2);
 
         vm.stopPrank();
     }
