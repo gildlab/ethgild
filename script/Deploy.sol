4fa4e014a25f4205aac0fd8798ff2b3f3fb542e4
thedavidmeister <thedavidmeister@gmail.com>
2024-06-11 15:49:00 +0400

    fmt

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -4,7 +4,10 @@ pragma solidity =0.8.17;
 import {Script} from "forge-std/Script.sol";
 import {OwnableOracle} from "contracts/concrete/OwnableOracle.sol";
 import {ERC20PriceOracleReceiptVault} from "contracts/vault/priceOracle/ERC20PriceOracleReceiptVault.sol";
-import {ERC20PriceOracleReceiptVaultFactory, ReceiptVaultFactoryConfig} from "contracts/vault/priceOracle/ERC20PriceOracleReceiptVaultFactory.sol";
+import {
+    ERC20PriceOracleReceiptVaultFactory,
+    ReceiptVaultFactoryConfig
+} from "contracts/vault/priceOracle/ERC20PriceOracleReceiptVaultFactory.sol";
 import {VaultConfig} from "contracts/vault/receipt/ReceiptVault.sol";
 import {ERC20PriceOracleVaultConfig} from "contracts/vault/priceOracle/ERC20PriceOracleReceiptVault.sol";
 import {ReceiptFactory} from "contracts/vault/receipt/ReceiptFactory.sol";
@@ -17,21 +20,26 @@ bytes32 constant DEPLOYMENT_SUITE_OWNABLE_ORACLE_VAULT = keccak256("ownable-orac
 /// then cross chain deployed to whatever mainnet is required, by users.
 contract Deploy is Script {
     function deployOwnableOracleVault(uint256 deploymentKey) internal {
-        ERC20PriceOracleReceiptVaultFactory factory = ERC20PriceOracleReceiptVaultFactory(vm.envAddress("ERC20_PRICE_ORACLE_RECEIPT_VAULT_FACTORY"));
+        ERC20PriceOracleReceiptVaultFactory factory =
+            ERC20PriceOracleReceiptVaultFactory(vm.envAddress("ERC20_PRICE_ORACLE_RECEIPT_VAULT_FACTORY"));
 
         vm.startBroadcast(deploymentKey);
 
         // Deploy OwnableOracle
         OwnableOracle oracle = new OwnableOracle();
 
-        factory.createChild(abi.encode(ERC20PriceOracleVaultConfig({
-            priceOracle: address(oracle),
-            vaultConfig: VaultConfig({
-                asset: vm.envAddress("ERC20_PRICE_ORACLE_RECEIPT_VAULT_ASSET"),
-                name: vm.envString("ERC20_PRICE_ORACLE_RECEIPT_VAULT_NAME"),
-                symbol: vm.envString("ERC20_PRICE_ORACLE_RECEIPT_VAULT_SYMBOL")
-            })
-        })));
+        factory.createChild(
+            abi.encode(
+                ERC20PriceOracleVaultConfig({
+                    priceOracle: address(oracle),
+                    vaultConfig: VaultConfig({
+                        asset: vm.envAddress("ERC20_PRICE_ORACLE_RECEIPT_VAULT_ASSET"),
+                        name: vm.envString("ERC20_PRICE_ORACLE_RECEIPT_VAULT_NAME"),
+                        symbol: vm.envString("ERC20_PRICE_ORACLE_RECEIPT_VAULT_SYMBOL")
+                    })
+                })
+            )
+        );
 
         vm.stopBroadcast();
     }
@@ -43,10 +51,9 @@ contract Deploy is Script {
         ReceiptFactory receiptFactory = new ReceiptFactory();
 
         // Deploy ERC20PriceOracleReceiptVaultFactory
-        ERC20PriceOracleReceiptVaultFactory factory = new ERC20PriceOracleReceiptVaultFactory(ReceiptVaultFactoryConfig({
-            implementation: address(implementation),
-            receiptFactory: address(receiptFactory)
-        }));
+        ERC20PriceOracleReceiptVaultFactory factory = new ERC20PriceOracleReceiptVaultFactory(
+            ReceiptVaultFactoryConfig({implementation: address(implementation), receiptFactory: address(receiptFactory)})
+        );
         (factory);
 
         vm.stopBroadcast();
@@ -58,8 +65,7 @@ contract Deploy is Script {
 
         if (suite == DEPLOYMENT_SUITE_ORACLE_VAULT_FACTORY) {
             deployOracleVaultFactory(deployerPrivateKey);
-        }
-        else if (suite == DEPLOYMENT_SUITE_OWNABLE_ORACLE_VAULT) {
+        } else if (suite == DEPLOYMENT_SUITE_OWNABLE_ORACLE_VAULT) {
             deployOwnableOracleVault(deployerPrivateKey);
         } else {
             revert("Unknown deployment suite");
