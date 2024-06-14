0384e4796ac5439197b93a2b49ca717b159b4ecc
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-14 15:36:45 +0400

    Update testRedeposit

--- test/foundry/src/concrete/vault/Redeposit.t.sol
+++ test/foundry/src/concrete/vault/Redeposit.t.sol
@@ -21,44 +21,52 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
         address sender, address owner, uint256 assets, uint256 shares, uint256 id, bytes receiptInformation
     );
 
+    /// Test redeposit function
     function testReDeposit(
         uint256 fuzzedKeyAlice,
-        uint256 aliceAssets,
-        bytes memory fuzzedReceiptInformation,
+        uint256 fuzzedKeyBob,
+        uint256 assets,
+        uint256 assetsToRedeposit,
+        bytes memory data,
         string memory assetName,
         string memory assetSymbol,
-        uint256 shareRatio
+        uint256 minShareRatio,
+        uint256 timestamp,
+        uint256 blockNumber
     ) external {
         // Ensure the fuzzed key is within the valid range for secp256k1
-        fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
-        address alice = vm.addr(fuzzedKeyAlice);
-        shareRatio = bound(shareRatio, 1, 1e18);
+        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+        address bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
+        minShareRatio = bound(minShareRatio, 0, 1e18);
+        timestamp = bound(timestamp, 1, type(uint32).max - 1); // Need to subtract 1 for the next bound
 
-        // Prank as Alice for the transaction
+        blockNumber = bound(blockNumber, 0, type(uint256).max);
+        vm.roll(blockNumber);
+        // Bound assets
+        assets = bound(assets, 1, type(uint256).max / 2);
+        assetsToRedeposit = bound(assetsToRedeposit, 1, type(uint256).max / 2);
+
+        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
+        // Prank as Alice to set roles
         vm.startPrank(alice);
 
-        // Start recording logs
-        vm.recordLogs();
-         OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
+        vault.grantRole(vault.DEPOSITOR(), bob);
+        vault.grantRole(vault.CERTIFIER(), bob);
 
-        //set upperBound for assets so it does not overflow while calculating fixedPointDiv or fixedPointMul
-        uint256 upperBound = type(uint256).max / 1e18;
-        // Assume that aliceAssets is less than totalSupply
-        aliceAssets = bound(aliceAssets, 1, upperBound);
+        // Prank as Bob for the transaction
+        vm.startPrank(bob);
 
-        vault.grantRole(vault.DEPOSITOR(), alice);
+        vm.warp(timestamp);
+        // Certify system till the current timestamp
+        vault.certify(timestamp, blockNumber, false, data);
 
-        // Divide alice assets to 3 to have enough assets for redeposit
-        uint256 assetsToDeposit = aliceAssets.fixedPointDiv(3, Math.Rounding.Up);
-        uint256 expectedShares = assetsToDeposit.fixedPointMul(1e18, Math.Rounding.Up);
-        vault.deposit(assetsToDeposit, alice, shareRatio, fuzzedReceiptInformation);
+        vault.deposit(assets, alice, minShareRatio, data);
 
-        // Set up the event expectation for DepositWithReceipt
-        vm.expectEmit(true, true, true, true);
-        emit DepositWithReceipt(alice, alice, assetsToDeposit, expectedShares, 1, fuzzedReceiptInformation);
+        vm.expectEmit(false, false, false, true);
+        emit DepositWithReceipt(bob, alice, assetsToRedeposit, assetsToRedeposit, 1, data);
 
-        // Redeposit same amount
-        vault.redeposit(assetsToDeposit, alice, 1, fuzzedReceiptInformation);
+        // Redeposit
+        vault.redeposit(assetsToRedeposit, alice, 1, data);
 
         vm.stopPrank();
     }
@@ -66,17 +74,17 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
     /// Test redeposit to someone else reverts with certification expired
     function testReDepositToSomeoneElseReverts(
         uint256 fuzzedKeyAlice,
-        uint256 aliceAssets,
+        uint256 assets,
         uint256 fuzzedKeyBob,
-        uint256 shareRatio,
-        bytes memory fuzzedReceiptInformation,
+        uint256 minShareRatio,
+        bytes memory data,
         string memory assetName,
         string memory assetSymbol
     ) external {
         // Ensure the fuzzed key is within the valid range for secp256k1
         fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
         address alice = vm.addr(fuzzedKeyAlice);
-        shareRatio = bound(shareRatio, 1, 1e18);
+        minShareRatio = bound(minShareRatio, 0, 1e18);
 
         // Ensure the fuzzed key is within the valid range for secp256k1
         fuzzedKeyBob = bound(fuzzedKeyBob, 1, SECP256K1_ORDER - 1);
@@ -88,18 +96,18 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
         vm.startPrank(alice);
         // Start recording logs
         vm.recordLogs();
-         OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
+        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
 
         //set upperBound for assets so it does not overflow while calculating fixedPointDiv or fixedPointMul
         uint256 upperBound = type(uint256).max / 1e18;
-        // Assume that aliceAssets is less than totalSupply
-        aliceAssets = bound(aliceAssets, 1, upperBound);
+        // Assume that assets is less than totalSupply
+        assets = bound(assets, 1, upperBound);
 
         vault.grantRole(vault.DEPOSITOR(), alice);
-        vault.deposit(aliceAssets, alice, shareRatio, fuzzedReceiptInformation);
+        vault.deposit(assets, alice, minShareRatio, data);
 
         vm.expectRevert(abi.encodeWithSelector(CertificationExpired.selector, address(0), bob, 0, 1));
-        vault.redeposit(aliceAssets, bob, 1, fuzzedReceiptInformation);
+        vault.redeposit(assets, bob, 1, data);
 
         vm.stopPrank();
     }
@@ -107,17 +115,17 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
     /// Test redeposit to someone else with Depositor role
     function testReDepositToSomeoneElse(
         uint256 fuzzedKeyAlice,
-        uint256 aliceAssets,
+        uint256 assets,
         uint256 fuzzedKeyBob,
-        uint256 shareRatio,
-        bytes memory fuzzedReceiptInformation,
+        uint256 minShareRatio,
+        bytes memory data,
         string memory assetName,
         string memory assetSymbol
     ) external {
         // Ensure the fuzzed key is within the valid range for secp256k1
         fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
         address alice = vm.addr(fuzzedKeyAlice);
-        shareRatio = bound(shareRatio, 1, 1e18);
+        minShareRatio = bound(minShareRatio, 0, 1e18);
 
         // Ensure the fuzzed key is within the valid range for secp256k1
         fuzzedKeyBob = bound(fuzzedKeyBob, 1, SECP256K1_ORDER - 1);
@@ -129,30 +137,30 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
         vm.startPrank(alice);
         // Start recording logs
         vm.recordLogs();
-         OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
+        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
 
         {
             //set upperBound for assets so it does not overflow while calculating fixedPointDiv or fixedPointMul
             uint256 upperBound = type(uint256).max / 1e18;
-            // Assume that aliceAssets is less than totalSupply
-            aliceAssets = bound(aliceAssets, 1, upperBound);
+            // Assume that assets is less than totalSupply
+            assets = bound(assets, 1, upperBound);
 
             vault.grantRole(vault.DEPOSITOR(), alice);
             vault.grantRole(vault.DEPOSITOR(), bob);
         }
 
         // Divide alice assets to 3 to have enough assets for redeposit
-        uint256 assetsToDeposit = aliceAssets.fixedPointDiv(3, Math.Rounding.Down);
+        uint256 assetsToDeposit = assets.fixedPointDiv(3, Math.Rounding.Down);
         uint256 expectedShares = assetsToDeposit.fixedPointMul(1e18, Math.Rounding.Up);
 
-        vault.deposit(assetsToDeposit, bob, shareRatio, fuzzedReceiptInformation);
+        vault.deposit(assetsToDeposit, bob, minShareRatio, data);
 
         // Set up the event expectation for DepositWithReceipt
         vm.expectEmit(true, true, true, true);
-        emit DepositWithReceipt(alice, bob, assetsToDeposit, expectedShares, 1, fuzzedReceiptInformation);
+        emit DepositWithReceipt(alice, bob, assetsToDeposit, expectedShares, 1, data);
 
         // Redeposit same amount
-        vault.redeposit(assetsToDeposit, bob, 1, fuzzedReceiptInformation);
+        vault.redeposit(assetsToDeposit, bob, 1, data);
 
         vm.stopPrank();
     }
@@ -160,10 +168,10 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
     /// Test redeposit to someone else While system is certified
     function testReDepositToSomeoneElseWhileCertified(
         uint256 fuzzedKeyAlice,
-        uint256 aliceAssets,
+        uint256 assets,
         uint256 fuzzedKeyBob,
-        uint256 shareRatio,
-        bytes memory fuzzedReceiptInformation,
+        uint256 minShareRatio,
+        bytes memory data,
         string memory assetName,
         string memory assetSymbol,
         uint256 certifyUntil,
@@ -177,7 +185,7 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
         fuzzedKeyBob = bound(fuzzedKeyBob, 1, SECP256K1_ORDER - 1);
         address bob = vm.addr(fuzzedKeyBob);
 
-        shareRatio = bound(shareRatio, 1, 1e18);
+        minShareRatio = bound(minShareRatio, 0, 1e18);
         referenceBlockNumber = bound(referenceBlockNumber, 1, block.number);
         certifyUntil = bound(certifyUntil, 1, 1e6 - 1);
 
@@ -186,13 +194,13 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
         // Prank as Alice for the transaction
         vm.startPrank(alice);
 
-         OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
+        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
 
         {
             //set upperBound for assets so it does not overflow while calculating fixedPointDiv or fixedPointMul
             uint256 upperBound = type(uint256).max / 1e18;
-            // Assume that aliceAssets is less than totalSupply
-            aliceAssets = bound(aliceAssets, 1, upperBound);
+            // Assume that assets is less than totalSupply
+            assets = bound(assets, 1, upperBound);
 
             vault.grantRole(vault.DEPOSITOR(), alice);
             vault.grantRole(vault.DEPOSITOR(), bob);
@@ -200,21 +208,21 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
         }
 
         // Divide alice assets to 3 to have enough assets for redeposit
-        uint256 assetsToDeposit = aliceAssets.fixedPointDiv(3, Math.Rounding.Down);
+        uint256 assetsToDeposit = assets.fixedPointDiv(3, Math.Rounding.Down);
 
         // Call the certify function
-        vault.certify(certifyUntil, referenceBlockNumber, false, fuzzedReceiptInformation);
+        vault.certify(certifyUntil, referenceBlockNumber, false, data);
 
-        vault.deposit(assetsToDeposit, bob, shareRatio, fuzzedReceiptInformation);
+        vault.deposit(assetsToDeposit, bob, minShareRatio, data);
 
         uint256 expectedShares = assetsToDeposit.fixedPointMul(1e18, Math.Rounding.Up);
 
         // Set up the event expectation for DepositWithReceipt
         vm.expectEmit(true, true, true, true);
-        emit DepositWithReceipt(alice, bob, assetsToDeposit, expectedShares, 1, fuzzedReceiptInformation);
+        emit DepositWithReceipt(alice, bob, assetsToDeposit, expectedShares, 1, data);
 
         // Redeposit same amount
-        vault.redeposit(assetsToDeposit, bob, 1, fuzzedReceiptInformation);
+        vault.redeposit(assetsToDeposit, bob, 1, data);
 
         vm.stopPrank();
     }
@@ -222,33 +230,33 @@ contract RedepositTest is OffchainAssetReceiptVaultTest {
     /// Test redeposit reverts on nonexistent receipt id
     function testReDepositToNonExistentReceipt(
         uint256 fuzzedKeyAlice,
-        uint256 aliceAssets,
-        uint256 shareRatio,
-        bytes memory fuzzedReceiptInformation,
+        uint256 assets,
+        uint256 minShareRatio,
+        bytes memory data,
         string memory assetName,
         string memory assetSymbol
     ) external {
         // Ensure the fuzzed key is within the valid range for secp256k1
         fuzzedKeyAlice = bound(fuzzedKeyAlice, 1, SECP256K1_ORDER - 1);
         address alice = vm.addr(fuzzedKeyAlice);
-        shareRatio = bound(shareRatio, 1, 1e18);
+        minShareRatio = bound(minShareRatio, 0, 1e18);
 
         // Prank as Alice for the transaction
         vm.startPrank(alice);
         // Start recording logs
         vm.recordLogs();
-         OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
+        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
 
         //set upperBound for assets so it does not overflow while calculating fixedPointDiv or fixedPointMul
         uint256 upperBound = type(uint256).max / 1e18;
-        // Assume that aliceAssets is less than totalSupply
-        aliceAssets = bound(aliceAssets, 1, upperBound);
+        // Assume that assets is less than totalSupply
+        assets = bound(assets, 1, upperBound);
 
         vault.grantRole(vault.DEPOSITOR(), alice);
-        vault.deposit(aliceAssets, alice, shareRatio, fuzzedReceiptInformation);
+        vault.deposit(assets, alice, minShareRatio, data);
 
         vm.expectRevert(abi.encodeWithSelector(InvalidId.selector, 0));
-        vault.redeposit(aliceAssets, alice, 0, fuzzedReceiptInformation);
+        vault.redeposit(assets, alice, 0, data);
 
         vm.stopPrank();
     }
