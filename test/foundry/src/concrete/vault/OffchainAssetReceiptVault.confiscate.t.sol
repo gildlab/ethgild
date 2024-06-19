c596436f88f7ced4939ee2eb972d7b12217e7960
ninokeldishvili <ninokeldishvili@gmail.com>
2024-06-19 13:04:18 +0400

    move  function checkConfiscateReceipt out of lib and remove lib

--- test/foundry/src/concrete/vault/OffchainAssetReceiptVault.confiscate.t.sol
+++ test/foundry/src/concrete/vault/OffchainAssetReceiptVault.confiscate.t.sol
@@ -3,7 +3,6 @@ pragma solidity =0.8.25;
 
 import {OffchainAssetReceiptVault} from "../../../../../contracts/concrete/vault/OffchainAssetReceiptVault.sol";
 import {OffchainAssetReceiptVaultTest, Vm} from "test/foundry/abstract/OffchainAssetReceiptVaultTest.sol";
-import {LibConfiscateChecker} from "test/foundry/lib/LibConfiscateChecker.sol";
 import {Receipt as ReceiptContract} from "../../../../../contracts/concrete/receipt/Receipt.sol";
 
 contract Confiscate is OffchainAssetReceiptVaultTest {
@@ -68,6 +67,32 @@ contract Confiscate is OffchainAssetReceiptVaultTest {
             vault.balanceOf(alice) == 0 && vault.balanceOf(bob) == initialBalanceBob + initialBalanceAlice;
 
         assertTrue(balancesChanged, "Balances should change");
+    }
+
+    /// Checks that balances change.
+    function checkConfiscateReceipt(
+        OffchainAssetReceiptVault vault,
+        ReceiptContract receipt,
+        address alice,
+        address bob,
+        uint256 id,
+        bytes memory data
+    ) internal {
+        uint256 initialBalanceAlice = receipt.balanceOf(alice, id);
+        uint256 initialBalanceBob = receipt.balanceOf(bob, id);
+
+        vm.expectEmit(false, false, false, true);
+        emit ConfiscateReceipt(bob, alice, 1, vault.balanceOf(alice), data);
+
+        vm.expectEmit(false, false, false, true);
+        emit TransferSingle(address(vault), alice, bob, 1, vault.balanceOf(alice));
+
+        vault.confiscateReceipt(alice, id, data);
+        uint256 balanceAfterAlice = receipt.balanceOf(alice, id);
+        uint256 balanceAfterBob = receipt.balanceOf(bob, id);
+
+        bool noBalanceChange = balanceAfterAlice == 0 && balanceAfterBob == initialBalanceBob + initialBalanceAlice;
+        assertTrue(noBalanceChange, "ConfiscateReceipt change balances");
     }
 
     /// Test to checks ConfiscateShares does not change balances on zero balance
@@ -160,56 +185,6 @@ contract Confiscate is OffchainAssetReceiptVaultTest {
         vm.stopPrank();
     }
 
-    /// Test to checks Confiscated amount is transferred
-    function testConfiscatedIsTransferred(
-        uint256 fuzzedKeyAlice,
-        uint256 fuzzedKeyBob,
-        uint256 minShareRatio,
-        uint256 assets,
-        string memory assetName,
-        string memory assetSymbol,
-        bytes memory data,
-        uint256 certifyUntil,
-        uint256 referenceBlockNumber,
-        uint256 blockNumber,
-        bool forceUntil
-    ) external {
-        minShareRatio = bound(minShareRatio, 0, 1e18);
-        // Ensure the fuzzed key is within the valid range for secp256k1
-        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
-        address bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
-
-        blockNumber = bound(blockNumber, 0, type(uint256).max);
-        vm.roll(blockNumber);
-
-        referenceBlockNumber = bound(referenceBlockNumber, 0, blockNumber);
-        certifyUntil = bound(certifyUntil, 1, type(uint32).max);
-
-        vm.assume(alice != bob);
-        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetSymbol);
-
-        // Prank as Alice to set roles
-        vm.startPrank(alice);
-        vault.grantRole(vault.CONFISCATOR(), bob);
-        vault.grantRole(vault.DEPOSITOR(), bob);
-        vault.grantRole(vault.CERTIFIER(), bob);
-
-        // Prank as Bob for transactions
-        vm.startPrank(bob);
-
-        // Call the certify function
-        vault.certify(certifyUntil, referenceBlockNumber, forceUntil, data);
-
-        // Assume that assets is less than uint256 max
-        assets = bound(assets, 1, type(uint256).max);
-
-        vault.deposit(assets, alice, minShareRatio, data);
-
-        checkConfiscateShares(vault, alice, bob, data);
-
-        vm.stopPrank();
-    }
-
     /// Test to checks ConfiscateReceipt does not change balances on zero balance
     function testConfiscateReceiptOnZeroBalance(
         uint256 fuzzedKeyAlice,
@@ -280,67 +255,7 @@ contract Confiscate is OffchainAssetReceiptVaultTest {
 
         vault.deposit(assets, alice, minShareRatio, data);
 
-        vm.expectEmit(false, false, false, true);
-        emit ConfiscateReceipt(bob, alice, 1, assets, data);
-
-        bool noBalanceChange = LibConfiscateChecker.checkConfiscateReceipt(vault, getReceipt(), alice, bob, 1, data);
-        assertTrue(noBalanceChange, "ConfiscateReceipt change balances");
-
-        // vault.confiscateReceipt(alice, 1, data);
-        vm.stopPrank();
-    }
-
-    /// Test to checks ConfiscatedReceipt amount is transferred
-    function testConfiscatedReceiptIsTransferred(
-        uint256 fuzzedKeyAlice,
-        uint256 fuzzedKeyBob,
-        uint256 minShareRatio,
-        uint256 assets,
-        string memory assetName,
-        bytes memory data,
-        uint256 certifyUntil,
-        uint256 referenceBlockNumber,
-        uint256 blockNumber,
-        bool forceUntil
-    ) external {
-        minShareRatio = bound(minShareRatio, 0, 1e18);
-        // Ensure the fuzzed key is within the valid range for secp256k1
-        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
-        address bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
-        vm.assume(alice != bob);
-
-        blockNumber = bound(blockNumber, 0, type(uint256).max);
-        vm.roll(blockNumber);
-
-        referenceBlockNumber = bound(referenceBlockNumber, 0, blockNumber);
-        certifyUntil = bound(certifyUntil, 1, type(uint32).max);
-
-        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetName);
-
-        // Prank as Alice to set roles
-        vm.startPrank(alice);
-        vault.grantRole(vault.CONFISCATOR(), bob);
-        vault.grantRole(vault.DEPOSITOR(), bob);
-        vault.grantRole(vault.CERTIFIER(), bob);
-
-        // Prank as Bob for the transaction
-        vm.startPrank(bob);
-
-        // Call the certify function
-        vault.certify(certifyUntil, referenceBlockNumber, forceUntil, data);
-
-        //set upperBound for assets so it does not overflow
-        uint256 upperBound = type(uint256).max / 1e18;
-        // Assume that assets is less than totalSupply
-        assets = bound(assets, 1, upperBound);
-
-        vault.deposit(assets, alice, minShareRatio, data);
-        vault.deposit(assets, alice, minShareRatio, data);
-
-        vm.expectEmit(false, false, false, true);
-        emit TransferSingle(address(vault), alice, bob, 1, assets);
-
-        vault.confiscateReceipt(alice, 1, data);
+        checkConfiscateReceipt(vault, getReceipt(), alice, bob, 1, data);
         vm.stopPrank();
     }
 }
