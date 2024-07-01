4ab6c7db2f3a67bdde6cc93d5f063a25370ceb09
ninokeldishvili <ninokeldishvili@gmail.com>
2024-07-01 11:36:59 +0400

    Test testReceiptTransfer to self with handler role

--- test/foundry/src/concrete/vault/OffchainAssetReceiptVault.handler.t.sol
+++ test/foundry/src/concrete/vault/OffchainAssetReceiptVault.handler.t.sol
@@ -41,6 +41,61 @@ contract OffchainAssetReceiptVaultHandlerTest is OffchainAssetReceiptVaultTest {
         return ReceiptContract(receiptAddress);
     }
 
+    /// Test testReceiptTransfer to self with handler role
+    function testReceiptTransferHandler(
+        uint256 fuzzedKeyAlice,
+        uint256 fuzzedKeyBob,
+        string memory assetName,
+        uint256 referenceBlockNumber,
+        uint256 certifyUntil,
+        uint256 futureTimeStamp,
+        bool forceUntil,
+        uint256 balance
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+        address bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
+
+        balance = bound(balance, 1, type(uint256).max); // Bound from one to avoid ZeroAssets
+        referenceBlockNumber = bound(referenceBlockNumber, 1, block.number);
+        certifyUntil = bound(certifyUntil, 1, type(uint32).max - 1); // substruct 1 for next bound
+
+        // Need setting future timestamp so system gets unsertified but transfer is possible
+        // due to a handler role
+        futureTimeStamp = bound(futureTimeStamp, certifyUntil + 1, type(uint32).max);
+
+        vm.assume(alice != bob);
+
+        // Start recording logs
+        vm.recordLogs();
+        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetName);
+        ReceiptContract receipt = getReceipt();
+
+        // Prank as Alice to grant roles
+        vm.startPrank(alice);
+
+        vault.grantRole(vault.CERTIFIER(), alice);
+        vault.grantRole(vault.HANDLER(), bob);
+        vault.grantRole(vault.DEPOSITOR(), alice);
+
+        // Call the certify function
+        vault.certify(certifyUntil, referenceBlockNumber, forceUntil, bytes(""));
+
+        // Cannot fuzz assets value due to variable limits
+        vault.deposit(balance, bob, 1, bytes(""));
+
+        vm.stopPrank();
+        vm.warp(futureTimeStamp);
+
+        // Prank as Bob
+        vm.startPrank(bob);
+        vault.authorizeReceiptTransfer(bob, bob);
+        receipt.safeTransferFrom(bob, bob, 1, balance, bytes(""));
+        assertEq(receipt.balanceOf(bob, 1), balance);
+
+        vm.stopPrank();
+    }
+
     /// Test testReceiptTransfer with Owner being a handler
     function testReceiptTransferHandlerOwner(
         uint256 fuzzedKeyAlice,
