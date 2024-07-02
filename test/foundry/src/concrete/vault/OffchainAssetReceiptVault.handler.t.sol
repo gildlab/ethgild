1a0265193278a229d159646cb9a98c574b372f87
ninokeldishvili <ninokeldishvili@gmail.com>
2024-07-02 14:23:56 +0400

    Create setUpAddressesAndBounds and setUpVault functions and update one test

--- test/foundry/src/concrete/vault/OffchainAssetReceiptVault.handler.t.sol
+++ test/foundry/src/concrete/vault/OffchainAssetReceiptVault.handler.t.sol
@@ -13,6 +13,36 @@ contract OffchainAssetReceiptVaultHandlerTest is OffchainAssetReceiptVaultTest {
         address sender, address owner, uint256 assets, uint256 shares, uint256 id, bytes receiptInformation
     );
 
+    function setUpAddressesAndBounds(
+        uint256 fuzzedKeyAlice,
+        uint256 fuzzedKeyBob,
+        uint256 fuzzedKeyJohn,
+        uint256 balance,
+        uint256 referenceBlockNumber,
+        uint256 certifyUntil
+    ) internal view returns (address alice, address bob, address john, uint256, uint256, uint256) {
+        alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+        bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
+        john = vm.addr((fuzzedKeyJohn % (SECP256K1_ORDER - 1)) + 1);
+
+        balance = bound(balance, 1, type(uint256).max); // Bound from one to avoid ZeroAssets
+        referenceBlockNumber = bound(referenceBlockNumber, 1, block.number);
+        certifyUntil = bound(certifyUntil, 1, type(uint32).max - 1); // substruct 1 for next bound
+
+        return (alice, bob, john, balance, referenceBlockNumber, certifyUntil);
+    }
+
+    function setUpVault(address alice, string memory assetName, string memory assetSymbol)
+        internal
+        returns (OffchainAssetReceiptVault vault, ReceiptContract receipt)
+    {
+        vm.recordLogs();
+        vault = createVault(alice, assetName, assetSymbol);
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+        receipt = getReceipt(logs);
+        return (vault, receipt);
+    }
+
     /// Test testReceiptTransfer to self with handler role
     function testReceiptTransferHandler(
         uint256 fuzzedKeyAlice,
@@ -24,13 +54,10 @@ contract OffchainAssetReceiptVaultHandlerTest is OffchainAssetReceiptVaultTest {
         bool forceUntil,
         uint256 balance
     ) external {
-        // Ensure the fuzzed key is within the valid range for secp256k1
-        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
-        address bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
-
-        balance = bound(balance, 1, type(uint256).max); // Bound from one to avoid ZeroAssets
-        referenceBlockNumber = bound(referenceBlockNumber, 1, block.number);
-        certifyUntil = bound(certifyUntil, 1, type(uint32).max - 1); // substruct 1 for next bound
+        address alice;
+        address bob;
+        (alice, bob,, balance, referenceBlockNumber, certifyUntil) =
+            setUpAddressesAndBounds(fuzzedKeyAlice, fuzzedKeyBob, 0, balance, referenceBlockNumber, certifyUntil);
 
         // Need setting future timestamp so system gets unsertified but transfer is possible
         // due to a handler role
@@ -38,12 +65,9 @@ contract OffchainAssetReceiptVaultHandlerTest is OffchainAssetReceiptVaultTest {
 
         vm.assume(alice != bob);
 
-        // Start recording logs
-        vm.recordLogs();
-        OffchainAssetReceiptVault vault = createVault(alice, assetName, assetName);
-        Vm.Log[] memory logs = vm.getRecordedLogs();
-
-        ReceiptContract receipt = getReceipt(logs);
+        OffchainAssetReceiptVault vault;
+        ReceiptContract receipt;
+        (vault, receipt) = setUpVault(alice, assetName, assetName);
 
         // Prank as Alice to grant roles
         vm.startPrank(alice);
@@ -55,7 +79,6 @@ contract OffchainAssetReceiptVaultHandlerTest is OffchainAssetReceiptVaultTest {
         // Call the certify function
         vault.certify(certifyUntil, referenceBlockNumber, forceUntil, bytes(""));
 
-        // Cannot fuzz assets value due to variable limits
         vault.deposit(balance, bob, 1, bytes(""));
 
         vm.stopPrank();
