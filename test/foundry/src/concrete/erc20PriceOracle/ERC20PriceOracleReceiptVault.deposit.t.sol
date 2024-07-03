95f576edc42befe535d9311aca94283b5d17aecd
ninokeldishvili <ninokeldishvili@gmail.com>
2024-07-03 16:45:15 +0400

    Test deposit function with zero assets - use overloaded deposit

--- test/foundry/src/concrete/erc20PriceOracle/ERC20PriceOracleReceiptVault.deposit.t.sol
+++ test/foundry/src/concrete/erc20PriceOracle/ERC20PriceOracleReceiptVault.deposit.t.sol
@@ -5,6 +5,7 @@ import {MinShareRatio, ZeroAssetsAmount, ZeroReceiver} from "../../../../../cont
 import {ERC20PriceOracleReceiptVault} from "../../../../../contracts/concrete/vault/ERC20PriceOracleReceiptVault.sol";
 import {ERC20PriceOracleReceiptVaultTest, Vm} from "test/foundry/abstract/ERC20PriceOracleReceiptVaultTest.sol";
 import {TwoPriceOracle, TwoPriceOracleConfig} from "../../../../../contracts/oracle/price/TwoPriceOracle.sol";
+import "forge-std/console.sol";
 
 contract ERC20PriceOracleReceiptVaultDepositTest is ERC20PriceOracleReceiptVaultTest {
     event DepositWithReceipt(
@@ -14,19 +15,19 @@ contract ERC20PriceOracleReceiptVaultDepositTest is ERC20PriceOracleReceiptVault
     /// Test deposit function with zero assets
     function testDepositWithZeroAssets(
         uint256 fuzzedKeyAlice,
-        uint256 fuzzedKeyBob,
         string memory assetName,
-        string memory assetSymbol
+        string memory assetSymbol,
+        bytes memory data
     ) external {
         // Ensure the fuzzed key is within the valid range for secp256k1
         address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
-        address bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
-        vm.assume(alice != bob);
         TwoPriceOracle twoPriceOracle = createTwoPriceOracle();
 
         ERC20PriceOracleReceiptVault vault = createVault(address(twoPriceOracle), assetName, assetSymbol);
 
+        uint256 oraclePrice = twoPriceOracle.price();
+
         vm.expectRevert(abi.encodeWithSelector(ZeroAssetsAmount.selector));
-        vault.deposit(0, bob);
+        vault.deposit(0, alice, oraclePrice, data);
     }
 }
