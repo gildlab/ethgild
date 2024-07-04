174138daabce24643012ed40bad9240f4a673638
ninokeldishvili <ninokeldishvili@gmail.com>
2024-07-04 12:21:52 +0400

    Test deposit function fix

--- test/foundry/src/concrete/erc20PriceOracle/ERC20PriceOracleReceiptVault.deposit.t.sol
+++ test/foundry/src/concrete/erc20PriceOracle/ERC20PriceOracleReceiptVault.deposit.t.sol
@@ -11,8 +11,6 @@ import {
     Math
 } from "rain.math.fixedpoint/lib/LibFixedPointDecimalArithmeticOpenZeppelin.sol";
 
-import "forge-std/console.sol";
-
 contract ERC20PriceOracleReceiptVaultDepositTest is ERC20PriceOracleReceiptVaultTest {
     using LibFixedPointDecimalArithmeticOpenZeppelin for uint256;
 
@@ -34,79 +32,82 @@ contract ERC20PriceOracleReceiptVaultDepositTest is ERC20PriceOracleReceiptVault
 
         ERC20PriceOracleReceiptVault vault;
         TestErc20 asset;
-
         (vault, asset) = createVault(address(twoPriceOracle), assetName, assetSymbol);
 
-        assets = bound(assets, 1, asset.totalSupply());
-        console.log("bla bla bla1 ",asset.totalSupply());
-        
-        uint256 oraclePrice = twoPriceOracle.price();
+        // Getting ZeroSharesAmount if bounded from 1
+        assets = bound(assets, 2, asset.totalSupply());
 
+        uint256 oraclePrice = twoPriceOracle.price();
         uint256 expectedShares = assets.fixedPointMul(oraclePrice, Math.Rounding.Down);
 
-        asset.transfer(alice, assets);
         vm.prank(alice);
-        console.log("blaaaaaaaa",expectedShares);
 
         asset.increaseAllowance(address(vault), asset.totalSupply());
+        vault.deposit(assets, alice, oraclePrice, data);
 
-        vault.deposit(1000, alice, oraclePrice, data);
+        // Assert that the total assets is equal to deposited assets
+        assertEqUint(vault.totalAssets(), assets);
+
+        // Assert that the total supply is equal to expectedShares
+        assertEqUint(vault.totalSupply(), expectedShares);
+        // Check alice balance
+        assertEqUint(vault.balanceOf(alice), expectedShares);
     }
 
-    // /// Test deposit function with zero assets
-    // function testDepositWithZeroAssets(
-    //     uint256 fuzzedKeyAlice,
-    //     string memory assetName,
-    //     string memory assetSymbol,
-    //     bytes memory data
-    // ) external {
-    //     // Ensure the fuzzed key is within the valid range for secp256k1
-    //     address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
-    //     TwoPriceOracle twoPriceOracle = createTwoPriceOracle();
+    /// Test deposit function with zero assets
+    function testDepositWithZeroAssets(
+        uint256 fuzzedKeyAlice,
+        string memory assetName,
+        string memory assetSymbol,
+        bytes memory data
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+        TwoPriceOracle twoPriceOracle = createTwoPriceOracle();
 
-    //     (ERC20PriceOracleReceiptVault vault, ) = createVault(address(twoPriceOracle), assetName, assetSymbol);
+        (ERC20PriceOracleReceiptVault vault,) = createVault(address(twoPriceOracle), assetName, assetSymbol);
 
-    //     uint256 oraclePrice = twoPriceOracle.price();
+        uint256 oraclePrice = twoPriceOracle.price();
 
-    //     vm.expectRevert(abi.encodeWithSelector(ZeroAssetsAmount.selector));
-    //     vault.deposit(0, alice, oraclePrice, data);
-    // }
+        vm.expectRevert(abi.encodeWithSelector(ZeroAssetsAmount.selector));
+        vault.deposit(0, alice, oraclePrice, data);
+    }
 
-    // /// Test deposit reverts with incorret price
-    // function testDepositWithIncorrectPrice(
-    //     uint256 fuzzedKeyAlice,
-    //     string memory assetName,
-    //     string memory assetSymbol,
-    //     bytes memory data,
-    //     uint256 assets
-    // ) external {
-    //     // Ensure the fuzzed key is within the valid range for secp256k1
-    //     address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
-    //     TwoPriceOracle twoPriceOracle = createTwoPriceOracle();
-    //     assets = bound(assets, 1, type(uint256).max);
-    //     (ERC20PriceOracleReceiptVault vault, ) = createVault(address(twoPriceOracle), assetName, assetSymbol);
+    /// Test deposit reverts with incorret price
+    function testDepositWithIncorrectPrice(
+        uint256 fuzzedKeyAlice,
+        string memory assetName,
+        string memory assetSymbol,
+        bytes memory data,
+        uint256 assets
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+        TwoPriceOracle twoPriceOracle = createTwoPriceOracle();
+        assets = bound(assets, 1, type(uint256).max);
+        (ERC20PriceOracleReceiptVault vault,) = createVault(address(twoPriceOracle), assetName, assetSymbol);
 
-    //     uint256 oraclePrice = twoPriceOracle.price();
+        uint256 oraclePrice = twoPriceOracle.price();
 
-    //     vm.expectRevert(abi.encodeWithSelector(MinShareRatio.selector, oraclePrice + 1, oraclePrice));
-    //     vault.deposit(assets, alice, oraclePrice + 1, data);
-    // }
+        vm.expectRevert(abi.encodeWithSelector(MinShareRatio.selector, oraclePrice + 1, oraclePrice));
+        vault.deposit(assets, alice, oraclePrice + 1, data);
+    }
 
-    // /// Test deposit reverts with zero receiver
-    // function testDepositWithZeroReceiver(
-    //     string memory assetName,
-    //     string memory assetSymbol,
-    //     bytes memory data,
-    //     uint256 assets
-    // ) external {
-    //     // Ensure the fuzzed key is within the valid range for secp256k1
-    //     TwoPriceOracle twoPriceOracle = createTwoPriceOracle();
-    //     assets = bound(assets, 1, type(uint256).max);
-    //     (ERC20PriceOracleReceiptVault vault, ) = createVault(address(twoPriceOracle), assetName, assetSymbol);
+    /// Test deposit reverts with zero receiver
+    function testDepositWithZeroReceiver(
+        string memory assetName,
+        string memory assetSymbol,
+        bytes memory data,
+        uint256 assets
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        TwoPriceOracle twoPriceOracle = createTwoPriceOracle();
+        assets = bound(assets, 1, type(uint256).max);
+        (ERC20PriceOracleReceiptVault vault,) = createVault(address(twoPriceOracle), assetName, assetSymbol);
 
-    //     uint256 oraclePrice = twoPriceOracle.price();
+        uint256 oraclePrice = twoPriceOracle.price();
 
-    //     vm.expectRevert();
-    //     vault.deposit(assets, address(0), oraclePrice, data);
-    // }
+        vm.expectRevert();
+        vault.deposit(assets, address(0), oraclePrice, data);
+    }
 }
