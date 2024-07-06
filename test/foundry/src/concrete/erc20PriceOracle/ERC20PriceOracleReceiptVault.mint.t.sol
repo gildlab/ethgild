54edec323d111210855c31bbf2e1a9b2e6339bcf
ninokeldishvili <ninokeldishvili@gmail.com>
2024-07-06 15:51:53 +0400

    Test mint to someone else

--- test/foundry/src/concrete/erc20PriceOracle/ERC20PriceOracleReceiptVault.mint.t.sol
+++ test/foundry/src/concrete/erc20PriceOracle/ERC20PriceOracleReceiptVault.mint.t.sol
@@ -70,56 +70,62 @@ contract ERC20PriceOracleReceiptVaultDepositTest is ERC20PriceOracleReceiptVault
     // }
 
     /// Test mint to someone else
-    // function testMintSomeoneElse(
-    //     uint256 fuzzedKeyAlice,
-    //     uint256 fuzzedKeyBob,
-    //     string memory assetName,
-    //     uint256 timestamp,
-    //     uint256 assets,
-    //     uint8 usdDecimals,
-    //     uint80 answeredInRound,
-    //     address erc20Address
-    // ) external {
-    //     // Ensure the fuzzed key is within the valid range for secp256
-    //     address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
-    //     address bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
-    //     vm.assume(erc20Address != address(0));
-    //     // Use common decimal bounds for price feeds
-    //     // Use 0-20 so we at least have some coverage higher than 18
-    //     usdDecimals = uint8(bound(usdDecimals, 0, 20));
-    //     // xauDecimals = uint8(bound(xauDecimals, 0, 20));
-    //     timestamp = bound(timestamp, 0, type(uint32).max);
+    function testMintSomeoneElse(
+        uint256 fuzzedKeyAlice,
+        uint256 fuzzedKeyBob,
+        string memory assetName,
+        uint256 timestamp,
+        uint256 assets,
+        uint8 usdDecimals,
+        uint80 answeredInRound,
+        address erc20Address
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256
+        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+        address bob = vm.addr((fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1);
+        vm.assume(erc20Address != address(0));
+        // Use common decimal bounds for price feeds
+        // Use 0-20 so we at least have some coverage higher than 18
+        usdDecimals = uint8(bound(usdDecimals, 0, 20));
+        timestamp = bound(timestamp, 0, type(uint32).max);
 
-    //     vm.warp(timestamp);
-    //     TwoPriceOracle twoPriceOracle = createTwoPriceOracle(usdDecimals, usdDecimals, timestamp, answeredInRound);
-    //     vm.startPrank(alice);
-    //     ERC20PriceOracleReceiptVault vault;
-    //     {
-    //         IERC20 asset;
-    //         (vault, asset) = createVault(address(twoPriceOracle), assetName, assetName, erc20Address);
+        vm.warp(timestamp);
+        TwoPriceOracle twoPriceOracle = createTwoPriceOracle(usdDecimals, usdDecimals, timestamp, answeredInRound);
+        vm.startPrank(alice);
+        ERC20PriceOracleReceiptVault vault;
+        {
+            IERC20 asset;
+            (vault, asset) = createVault(address(twoPriceOracle), assetName, assetName, erc20Address);
 
-    //         vm.mockCall(address(asset), abi.encodeWithSelector(IERC20.totalSupply.selector), abi.encode(1e18));
+            vm.mockCall(address(asset), abi.encodeWithSelector(IERC20.totalSupply.selector), abi.encode(1e18));
 
-    //         // Ensure Alice has enough balance and allowance
-    //         vm.mockCall(address(asset), abi.encodeWithSelector(IERC20.balanceOf.selector, alice), abi.encode(assets));
+            // Ensure Alice has enough balance and allowance
+            vm.mockCall(address(asset), abi.encodeWithSelector(IERC20.balanceOf.selector, alice), abi.encode(assets));
 
-    //         uint256 totalSupply = asset.totalSupply();
-    //         // Getting ZeroSharesAmount if bounded from 1
-    //         assets = bound(assets, 2, totalSupply);
+            uint256 totalSupply = asset.totalSupply();
+            assets = bound(assets, 2, totalSupply);
 
-    //         vm.mockCall(
-    //             address(asset),
-    //             abi.encodeWithSelector(IERC20.transferFrom.selector, alice, vault, assets),
-    //             abi.encode(true)
-    //         );
-    //     }
-    //     uint256 oraclePrice = twoPriceOracle.price();
-    //     uint256 shares = assets.fixedPointMul(oraclePrice, Math.Rounding.Down);
+            vm.mockCall(
+                address(asset),
+                abi.encodeWithSelector(IERC20.transferFrom.selector, alice, vault, assets),
+                abi.encode(true)
+            );
 
-    //     vault.mint(shares, bob, oraclePrice, bytes(""));
-    //     // Check balance
-    //     assertEqUint(vault.balanceOf(bob), shares);
-    // }
+            // Ensure sufficient allowance is set
+            vm.mockCall(
+                address(asset),
+                abi.encodeWithSelector(IERC20.allowance.selector, alice, address(vault)),
+                abi.encode(assets)
+            );
+        }
+        uint256 oraclePrice = twoPriceOracle.price();
+        uint256 shares = assets.fixedPointMul(oraclePrice, Math.Rounding.Down);
+
+        vault.mint(shares, bob, oraclePrice, bytes(""));
+
+        // Check balance
+        assertEqUint(vault.balanceOf(bob), shares);
+    }
 
     /// Test mint function with zero shares
     function testMintWithZeroShares(
