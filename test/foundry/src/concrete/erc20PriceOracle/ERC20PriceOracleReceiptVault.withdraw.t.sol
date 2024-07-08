8800388085a428a3c5b8160f4a6cf53cd98c0c3c
ninokeldishvili <ninokeldishvili@gmail.com>
2024-07-08 13:53:13 +0400

    Test Withdraw function reverts on zero owner

--- test/foundry/src/concrete/erc20PriceOracle/ERC20PriceOracleReceiptVault.withdraw.t.sol
+++ test/foundry/src/concrete/erc20PriceOracle/ERC20PriceOracleReceiptVault.withdraw.t.sol
@@ -13,7 +13,7 @@ import {
 } from "rain.math.fixedpoint/lib/LibFixedPointDecimalArithmeticOpenZeppelin.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {Receipt as ReceiptContract} from "../../../../../contracts/concrete/receipt/Receipt.sol";
-import {ZeroAssetsAmount, ZeroReceiver} from "../../../../../contracts/abstract/ReceiptVault.sol";
+import {ZeroAssetsAmount, ZeroReceiver, ZeroOwner} from "../../../../../contracts/abstract/ReceiptVault.sol";
 
 import "forge-std/console.sol";
 
@@ -89,7 +89,12 @@ contract ERC20PriceOracleReceiptVaultWithdrawTest is ERC20PriceOracleReceiptVaul
         bytes memory data,
         bytes memory expectedRevertData
     ) internal {
-        uint256 initialBalanceOwner = receipt.balanceOf(owner, id);
+        uint256 initialBalanceOwner;
+        uint256 balanceAfterOwner;
+
+        if (owner != address(0)) {
+            initialBalanceOwner = receipt.balanceOf(owner, id);
+        }
 
         // Check if expectedRevertData is provided
         if (expectedRevertData.length > 0) {
@@ -100,7 +105,9 @@ contract ERC20PriceOracleReceiptVaultWithdrawTest is ERC20PriceOracleReceiptVaul
         // Call withdraw function
         vault.withdraw(assets, receiver, owner, id, data);
 
-        uint256 balanceAfterOwner = receipt.balanceOf(owner, id);
+        if (owner != address(0)) {
+            balanceAfterOwner = receipt.balanceOf(owner, id);
+        }
         assertEq(balanceAfterOwner, initialBalanceOwner);
     }
 
@@ -251,6 +258,62 @@ contract ERC20PriceOracleReceiptVaultWithdrawTest is ERC20PriceOracleReceiptVaul
             receipt,
             bytes(""),
             abi.encodeWithSelector(ZeroReceiver.selector)
+        );
+    }
+
+    /// Test Withdraw function reverts on zero owner
+    function testWithdrawRevertsOnZeroOwner(
+        uint256 fuzzedKeyAlice,
+        string memory assetName,
+        uint256 timestamp,
+        uint256 assets,
+        uint8 xauDecimals,
+        uint8 usdDecimals,
+        uint80 answeredInRound
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256
+        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+        // Use common decimal bounds for price feeds
+        // Use 0-20 so we at least have some coverage higher than 18
+        usdDecimals = uint8(bound(usdDecimals, 0, 20));
+        xauDecimals = uint8(bound(xauDecimals, 0, 20));
+        timestamp = bound(timestamp, 0, type(uint32).max);
+
+        vm.warp(timestamp);
+        TwoPriceOracle twoPriceOracle = createTwoPriceOracle(usdDecimals, usdDecimals, timestamp, answeredInRound);
+        vm.startPrank(alice);
+        // Start recording logs
+        vm.recordLogs();
+        ERC20PriceOracleReceiptVault vault = createVault(address(twoPriceOracle), assetName, assetName);
+        ReceiptContract receipt = getReceipt();
+
+        vm.mockCall(address(iAsset), abi.encodeWithSelector(IERC20.totalSupply.selector), abi.encode(1e18));
+        // Ensure Alice has enough balance and allowance
+        vm.mockCall(address(iAsset), abi.encodeWithSelector(IERC20.balanceOf.selector, alice), abi.encode(assets));
+
+        uint256 totalSupply = iAsset.totalSupply();
+        // Getting ZeroSharesAmount if bounded from 1
+        assets = bound(assets, 2, totalSupply);
+        vm.mockCall(
+            address(iAsset),
+            abi.encodeWithSelector(IERC20.transferFrom.selector, alice, vault, assets),
+            abi.encode(true)
+        );
+
+        uint256 oraclePrice = twoPriceOracle.price();
+
+        vault.deposit(assets, alice, oraclePrice, bytes(""));
+        uint256 availableReceiptBalance = receipt.balanceOf(alice, oraclePrice);
+
+        checkNoBalanceChange(
+            vault,
+            alice,
+            address(0),
+            oraclePrice,
+            availableReceiptBalance,
+            receipt,
+            bytes(""),
+            abi.encodeWithSelector(ZeroOwner.selector)
         );
     }
 }
