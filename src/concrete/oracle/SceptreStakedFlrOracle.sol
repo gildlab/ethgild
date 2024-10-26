aa56f269294a465fa955a741d11297b14bb6ed57
thedavidmeister <thedavidmeister@gmail.com>
2024-10-26 21:40:31 +0400

    use lib staked

--- src/concrete/oracle/SceptreStakedFlrOracle.sol
+++ src/concrete/oracle/SceptreStakedFlrOracle.sol
@@ -3,19 +3,14 @@
 pragma solidity =0.8.25;
 
 import {IPriceOracleV2} from "../../interface/IPriceOracleV2.sol";
-import {IStakedFlr} from "rain.flare/interface/IStakedFlr.sol";
 import {Address} from "openzeppelin-contracts/contracts/utils/Address.sol";
+import {LibSceptreStakedFlare} from "rain.flare/lib/sflr/LibSceptreStakedFlare.sol";
 
 contract SceptreStakedFlrOracle is IPriceOracleV2 {
-    IStakedFlr public immutable iStakedFlr;
-
-    constructor(IStakedFlr stakedFlr) {
-        iStakedFlr = stakedFlr;
-    }
 
     /// @inheritdoc IPriceOracleV2
     function price() external payable override returns (uint256) {
-        uint256 val = iStakedFlr.getSharesByPooledFlr(1e18);
+        uint256 val = LibSceptreStakedFlare.getSFLRPerFLR18();
         Address.sendValue(payable(msg.sender), address(this).balance);
         return val;
     }
