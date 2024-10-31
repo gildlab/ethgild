30ea63c0c184403b1a7de244ab4207c0bb0f1401
ninokeldishvili <ninokeldishvili@gmail.com>
2024-10-31 12:35:19 +0400

    Update ReceiptFactoryTest

--- test/abstract/ReceiptFactoryTest.sol
+++ test/abstract/ReceiptFactoryTest.sol
@@ -5,25 +5,24 @@ pragma solidity ^0.8.25;
 import {ICloneableFactoryV2} from "rain.factory/interface/ICloneableFactoryV2.sol";
 import {CloneFactory} from "rain.factory/concrete/CloneFactory.sol";
 import {TestReceipt} from "test/concrete/TestReceipt.sol";
+import {Test, Vm} from "forge-std/Test.sol";
+import {Receipt as ReceiptContract} from "src/concrete/receipt/Receipt.sol";
 
-contract TestReceiptFactory {
-    ICloneableFactoryV2 public immutable factory;
-    address public immutable receiptImplementation;
+contract ReceiptFactoryTest is Test {
+    ICloneableFactoryV2 internal immutable iFactory;
+    ReceiptContract internal immutable receiptImplementation;
 
-    /// @notice Initialize the factory and receipt implementation addresses
-    /// @param _factory The address of the CloneFactory contract
-    /// @param _receiptImplementation The address of the implementation for TestReceipt
-    constructor(address _factory, address _receiptImplementation) {
-        factory = ICloneableFactoryV2(_factory);
-        receiptImplementation = _receiptImplementation;
+    constructor(address factoryAddress) {
+        iFactory = ICloneableFactoryV2(factoryAddress);
+        receiptImplementation = new ReceiptContract();
     }
 
     /// @notice Creates a new TestReceipt clone with the specified owner
     /// @param owner The address to set as the owner of the new TestReceipt
     /// @return The address of the newly created TestReceipt clone
     function createReceipt(address owner) external returns (TestReceipt) {
-        // Clone TestReceipt using the factory
-        address clone = factory.clone(receiptImplementation, abi.encode(owner));
+        // Clone TestReceipt using the factory and initialize it with the owner
+        address clone = iFactory.clone(address(receiptImplementation), abi.encode(owner));
 
         // Return the clone cast to TestReceipt type
         return TestReceipt(clone);
