75f4a11dd4b43cc67e0eaca428ef3636428fde6f
ninokeldishvili <ninokeldishvili@gmail.com>
2024-11-01 15:37:50 +0400

    Update testUnauthorizedTransferOwnerTransferFrom

--- test/src/concrete/receipt/Receipt.t.sol
+++ test/src/concrete/receipt/Receipt.t.sol
@@ -163,7 +163,6 @@ contract ReceiptTest is ReceiptFactoryTest {
         receiptOwner.setFrom(address(0));
         receiptOwner.setTo(alice);
 
-        vm.startPrank(alice);
         receiptOwner.ownerMint(receipt, alice, id, amount, fuzzedReceiptInformation);
         uint256 receiptBalance = receipt.balanceOf(alice, id);
         transferAmount = bound(transferAmount, receiptBalance + 1, type(uint256).max);
@@ -190,17 +189,18 @@ contract ReceiptTest is ReceiptFactoryTest {
         amount = bound(amount, 1, type(uint256).max);
         id = bound(id, 0, type(uint256).max);
 
-        TestReceipt receipt = new TestReceipt();
+        TestReceipt receipt = createReceipt(alice);
         TestReceiptOwner receiptOwner = new TestReceiptOwner();
 
+        vm.startPrank(alice);
+
         // Set the receipt owner
-        receipt.setOwner(address(receiptOwner));
+        receipt.transferOwnership(address(receiptOwner));
 
         // Set the authorized 'from' and 'to' addresses in receiptOwner
         receiptOwner.setFrom(address(0));
         receiptOwner.setTo(alice);
 
-        vm.startPrank(alice);
         receiptOwner.ownerMint(receipt, alice, id, amount, fuzzedReceiptInformation);
         uint256 receiptBalance = receipt.balanceOf(alice, id);
 
