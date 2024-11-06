c765b338b0b1d7b43b334a7fc4a6463e434119f3
ninokeldishvili <ninokeldishvili@gmail.com>
2024-11-06 10:46:18 +0400

    Create ERC20Standard.t.sol and add testERC20NameSymbolDecimals

---
+++ test/src/concrete/vault/ERC20Standard.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
+pragma solidity =0.8.25;
+
+import {
+    ERC20PriceOracleReceiptVault,
+    ERC20PriceOracleReceiptVaultConfig
+} from "src/concrete/vault/ERC20PriceOracleReceiptVault.sol";
+import {ERC20PriceOracleReceiptVaultTest, Vm} from "test/abstract/ERC20PriceOracleReceiptVaultTest.sol";
+import {IPriceOracleV2} from "src/interface/IPriceOracleV2.sol";
+
+contract ERC20StandardTest is ERC20PriceOracleReceiptVaultTest {
+    /// Test ERC20PriceOracleReceiptVault is constracted
+    function testERC20NameSymbolDecimals(uint256 fuzzedKeyAlice, string memory assetName, string memory assetSymbol)
+        external
+    {
+        // Ensure the fuzzed key is within the valid range for secp256
+        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+
+        IPriceOracleV2 vaultPriceOracle =
+            IPriceOracleV2(payable(address(uint160(uint256(keccak256("twoPriceOracle"))))));
+        vm.startPrank(alice);
+
+        ERC20PriceOracleReceiptVault vault = createVault(vaultPriceOracle, assetName, assetSymbol);
+
+        assert(address(vault) != address(0));
+        assertEq(keccak256(bytes(vault.name())), keccak256(bytes(assetName)));
+        assertEq(keccak256(bytes(vault.symbol())), keccak256(bytes(assetSymbol)));
+        assertEq(vault.decimals(), 18);
+    }
+}
