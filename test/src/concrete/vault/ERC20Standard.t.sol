215bde4c64e1d3194793c61714d150eabb515553
ninokeldishvili <ninokeldishvili@gmail.com>
2024-11-06 11:11:07 +0400

    Add testTotalSupplyAndBalanceOf

--- test/src/concrete/vault/ERC20Standard.t.sol
+++ test/src/concrete/vault/ERC20Standard.t.sol
@@ -8,8 +8,15 @@ import {
 } from "src/concrete/vault/ERC20PriceOracleReceiptVault.sol";
 import {ERC20PriceOracleReceiptVaultTest, Vm} from "test/abstract/ERC20PriceOracleReceiptVaultTest.sol";
 import {IPriceOracleV2} from "src/interface/IPriceOracleV2.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {
+    LibFixedPointDecimalArithmeticOpenZeppelin,
+    Math
+} from "rain.math.fixedpoint/lib/LibFixedPointDecimalArithmeticOpenZeppelin.sol";
 
 contract ERC20StandardTest is ERC20PriceOracleReceiptVaultTest {
+    using LibFixedPointDecimalArithmeticOpenZeppelin for uint256;
+
     /// Test ERC20 name symbol and decimals
     function testERC20NameSymbolDecimals(uint256 fuzzedKeyAlice, string memory assetName, string memory assetSymbol)
         external
@@ -27,5 +34,43 @@ contract ERC20StandardTest is ERC20PriceOracleReceiptVaultTest {
         assertEq(keccak256(bytes(vault.name())), keccak256(bytes(assetName)));
         assertEq(keccak256(bytes(vault.symbol())), keccak256(bytes(assetSymbol)));
         assertEq(vault.decimals(), 18);
+    }
+
+    /// Test ERC20 totalSupply and balanceOf
+    function testERC20TotalSupplyAndBalanceOf(
+        uint256 fuzzedKeyAlice,
+        string memory assetName,
+        uint256 assets,
+        uint256 oraclePrice
+    ) external {
+        // Ensure the fuzzed key is within the valid range for secp256
+        address alice = vm.addr((fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1);
+
+        oraclePrice = bound(oraclePrice, 0.01e18, 100e18);
+        setVaultOraclePrice(oraclePrice);
+
+        vm.startPrank(alice);
+
+        ERC20PriceOracleReceiptVault vault = createVault(iVaultOracle, assetName, assetName);
+        {
+            // Ensure Alice has enough balance and allowance
+            vm.mockCall(address(iAsset), abi.encodeWithSelector(IERC20.balanceOf.selector, alice), abi.encode(assets));
+
+            assets = bound(assets, 1, type(uint128).max);
+            vm.assume(assets.fixedPointMul(oraclePrice, Math.Rounding.Down) > 0);
+
+            vm.mockCall(
+                address(iAsset),
+                abi.encodeWithSelector(IERC20.transferFrom.selector, alice, vault, assets),
+                abi.encode(true)
+            );
+        }
+
+        uint256 expectedShares = assets.fixedPointMul(oraclePrice, Math.Rounding.Down);
+        vault.deposit(assets, alice, oraclePrice, bytes(""));
+        // Assert that the total supply is equal to expectedShares
+        assertEqUint(vault.totalSupply(), expectedShares);
+        // Check alice balance
+        assertEqUint(vault.balanceOf(alice), expectedShares);
     }
 }
