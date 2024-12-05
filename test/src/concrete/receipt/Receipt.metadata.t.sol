82b3c60d7804b63415d8f012649b05f95525607a
thedavidmeister <thedavidmeister@gmail.com>
2024-12-05 18:11:08 +0400

    lint test

--- test/src/concrete/receipt/Receipt.metadata.t.sol
+++ test/src/concrete/receipt/Receipt.metadata.t.sol
@@ -6,7 +6,7 @@ import {ReceiptFactoryTest} from "test/abstract/ReceiptFactoryTest.sol";
 import {TestReceiptOwner} from "test/concrete/TestReceiptOwner.sol";
 import {TestReceipt} from "test/concrete/TestReceipt.sol";
 import {Base64} from "solady/utils/Base64.sol";
-import {Receipt, RECEIPT_METADATA_DATA_URI, DATA_URI_BASE64_PREFIX} from "src/concrete/receipt/Receipt.sol";
+import {Receipt, RECEIPT_METADATA_DATA_URI, DATA_URI_BASE64_PREFIX, RECEIPT_NAME} from "src/concrete/receipt/Receipt.sol";
 
 contract ReceiptMetadataTest is ReceiptFactoryTest {
     struct Metadata {
@@ -39,7 +39,7 @@ contract ReceiptMetadataTest is ReceiptFactoryTest {
         Metadata memory metadataJson = abi.decode(uriJsonData, (Metadata));
         assertEq(metadataJson.description, "A receipt for a ReceiptVault.");
         assertEq(metadataJson.decimals, 18);
-        assertEq(metadataJson.name, "Receipt");
+        assertEq(metadataJson.name, RECEIPT_NAME);
     }
 
     function testReceiptName() external {
@@ -47,7 +47,7 @@ contract ReceiptMetadataTest is ReceiptFactoryTest {
         TestReceiptOwner mockOwner = new TestReceiptOwner();
         TestReceipt receipt = createReceipt(address(mockOwner));
 
-        assertEq(receipt.name(), "Receipt");
+        assertEq(receipt.name(), RECEIPT_NAME);
     }
 
     function testReceiptSymbol() external {
