e072e6184c192df572432b70e9fff300ba6d51ea
thedavidmeister <thedavidmeister@gmail.com>
2024-12-07 19:11:36 +0400

    update license entity

--- test/src/concrete/vault/OffchainAssetReceiptVault.snapshot.t.sol
+++ test/src/concrete/vault/OffchainAssetReceiptVault.snapshot.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LicenseRef-DCL-1.0
-// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
+// SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
 pragma solidity =0.8.25;
 
 import {OffchainAssetReceiptVaultTest, Vm} from "../../../abstract/OffchainAssetReceiptVaultTest.sol";
