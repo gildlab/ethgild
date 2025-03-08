4bec2bfb90541ead8ac475c4727ddf0378224c1e
thedavidmeister <thedavidmeister@gmail.com>
2025-03-08 22:53:55 +0400

    authorize test

---
+++ test/src/concrete/authorize/OffchainAssetReceiptVaultAuthorizerV1.authorize.t.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 Rain Open Source Software Ltd
+pragma solidity =0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+
+import {
+    OffchainAssetReceiptVaultAuthorizerV1,
+    OffchainAssetReceiptVaultAuthorizerV1Config,
+    TRANSFER_SHARES,
+    TRANSFER_RECEIPT,
+    CERTIFY,
+    ZeroInitialAdmin,
+    ZeroAuthorizee,
+    Unauthorized,
+    CONFISCATE_RECEIPT,
+    CONFISCATE_SHARES,
+    DEPOSIT,
+    WITHDRAW,
+    FREEZE_HANDLER
+} from "src/concrete/authorize/OffchainAssetReceiptVaultAuthorizerV1.sol";
+import {CloneFactory} from "rain.factory/concrete/CloneFactory.sol";
+
+contract OffchainAssetReceiptVaultAuthorizerV1AuthorizeTest is Test {
+    function testOffchainAssetReceiptVaultAuthorizerV1AuthorizeUnauthorized(
+        address initialAdmin,
+        address authorizee,
+        address user,
+        bytes32 permission,
+        bytes memory data
+    ) external {
+        vm.assume(initialAdmin != address(0));
+        vm.assume(authorizee != address(0));
+        vm.assume(initialAdmin != authorizee);
+
+        vm.assume(permission != TRANSFER_SHARES);
+        vm.assume(permission != TRANSFER_RECEIPT);
+
+        OffchainAssetReceiptVaultAuthorizerV1 authorizerImplementation = new OffchainAssetReceiptVaultAuthorizerV1();
+
+        OffchainAssetReceiptVaultAuthorizerV1Config memory config =
+            OffchainAssetReceiptVaultAuthorizerV1Config({initialAdmin: initialAdmin, authorizee: authorizee});
+
+        CloneFactory factory = new CloneFactory();
+        OffchainAssetReceiptVaultAuthorizerV1 authorizer =
+            OffchainAssetReceiptVaultAuthorizerV1(factory.clone(address(authorizerImplementation), abi.encode(config)));
+
+        vm.startPrank(authorizee);
+        vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, user, permission, data));
+        authorizer.authorize(user, permission, data);
+        vm.stopPrank();
+    }
+
+    function testOffchainAssetReceiptVaultAuthorizerV1AuthorizeAuthorized(
+        address initialAdmin,
+        address authorizee,
+        address user,
+        bytes memory data
+    ) external {
+        vm.assume(initialAdmin != address(0));
+        vm.assume(authorizee != address(0));
+        vm.assume(initialAdmin != authorizee);
+
+        OffchainAssetReceiptVaultAuthorizerV1 authorizerImplementation = new OffchainAssetReceiptVaultAuthorizerV1();
+
+        OffchainAssetReceiptVaultAuthorizerV1Config memory config =
+            OffchainAssetReceiptVaultAuthorizerV1Config({initialAdmin: initialAdmin, authorizee: authorizee});
+
+        CloneFactory factory = new CloneFactory();
+        OffchainAssetReceiptVaultAuthorizerV1 authorizer =
+            OffchainAssetReceiptVaultAuthorizerV1(factory.clone(address(authorizerImplementation), abi.encode(config)));
+
+        bytes32[] memory roles = new bytes32[](6);
+        roles[0] = CERTIFY;
+        roles[1] = CONFISCATE_SHARES;
+        roles[2] = CONFISCATE_RECEIPT;
+        roles[3] = DEPOSIT;
+        roles[4] = WITHDRAW;
+        roles[5] = FREEZE_HANDLER;
+
+        for (uint256 i = 0; i < roles.length; i++) {
+            vm.assertTrue(!authorizer.hasRole(roles[i], user));
+            vm.expectRevert(abi.encodeWithSelector(Unauthorized.selector, user, roles[i], data));
+            authorizer.authorize(user, roles[i], data);
+            vm.startPrank(initialAdmin);
+            authorizer.grantRole(roles[i], user);
+            vm.stopPrank();
+            authorizer.authorize(user, roles[i], data);
+        }
+    }
+}
