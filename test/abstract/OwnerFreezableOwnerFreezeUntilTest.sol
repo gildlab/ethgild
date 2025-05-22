f418f4ee42f44c4540f0caa8f585f992c8c12211
thedavidmeister <thedavidmeister@gmail.com>
2025-05-23 01:17:27 +0400

    wip on owner freezable tests

--- test/abstract/OwnerFreezableOwnerFreezeUntilTest.sol
+++ test/abstract/OwnerFreezableOwnerFreezeUntilTest.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.25;
 
 import {Test} from "forge-std/Test.sol";
 import {OwnerFreezable, IOwnerFreezableV1} from "src/abstract/OwnerFreezable.sol";
+import {MathUpgradeable as Math} from "openzeppelin-contracts-upgradeable/contracts/utils/math/MathUpgradeable.sol";
 
 abstract contract OwnerFreezableOwnerFreezeUntilTest is Test {
+    using Math for uint256;
+
     IOwnerFreezableV1 internal sOwnerFreezable;
     address internal sAlice;
     address internal sBob;
@@ -29,6 +32,31 @@ abstract contract OwnerFreezableOwnerFreezeUntilTest is Test {
         sOwnerFreezable.ownerFreezeUntil(freezeUntil);
     }
 
+    /// Freezing twice increases the freeze until time if the second time is equal
+    /// or greater than the first.
+    function testOwnerFreezableFreezingIncrement(uint256 a, uint256 b) external {
+        b = bound(b, a, type(uint256).max);
+        vm.prank(sAlice);
+        sOwnerFreezable.ownerFreezeUntil(a);
+        assertEq(sOwnerFreezable.ownerFrozenUntil(), a);
+
+        vm.prank(sAlice);
+        sOwnerFreezable.ownerFreezeUntil(b);
+        assertEq(sOwnerFreezable.ownerFrozenUntil(), b);
+    }
+
+    /// Freezing twice is a noop if the second time is less than or equal to the first.
+    function testOwnerFreezableFreezingDecrement(uint256 a, uint256 b) external {
+        b = bound(b, 0, a);
+        vm.prank(sAlice);
+        sOwnerFreezable.ownerFreezeUntil(a);
+        assertEq(sOwnerFreezable.ownerFrozenUntil(), a);
+
+        vm.prank(sAlice);
+        sOwnerFreezable.ownerFreezeUntil(b);
+        assertEq(sOwnerFreezable.ownerFrozenUntil(), a);
+    }
+
     /// Freezing many times works if all times increase.
     function testOwnerFreezableFreezingManyIncreasing(uint32[] memory times) external {
         uint256 freezeUntil = 0;
@@ -38,6 +66,17 @@ abstract contract OwnerFreezableOwnerFreezeUntilTest is Test {
             vm.prank(sAlice);
             sOwnerFreezable.ownerFreezeUntil(freezeUntil);
             assertEq(sOwnerFreezable.ownerFrozenUntil(), freezeUntil);
+        }
+    }
+
+    /// Freezing times in the past are noops.
+    function testOwnerFreezableFreezingMany(uint256[] memory freezeUntils) external {
+        uint256 highwater = 0;
+        for (uint256 i; i < freezeUntils.length; i++) {
+            highwater = freezeUntils[i].max(highwater);
+            vm.prank(sAlice);
+            sOwnerFreezable.ownerFreezeUntil(freezeUntils[i]);
+            assertEq(sOwnerFreezable.ownerFrozenUntil(), highwater);
         }
     }
 }
