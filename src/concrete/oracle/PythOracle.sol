d5dc44a12293799048059a71d8670cf02eedcf53
thedavidmeister <thedavidmeister@gmail.com>
2025-10-28 13:56:53 +0400

    constructor for pyth oracle

--- src/concrete/oracle/PythOracle.sol
+++ src/concrete/oracle/PythOracle.sol
@@ -9,10 +9,26 @@ import {LibDecimalFloat, Float} from "rain.math.float/lib/LibDecimalFloat.sol";
 
 error NonPositivePrice(int256 price);
 
+struct PythOracleConfig {
+    bytes32 priceFeedId;
+    uint256 staleAfter;
+    IPyth pythContract;
+}
+
 contract PythOracle is PriceOracleV2 {
+    event Construction(address sender, PythOracleConfig config);
+
     bytes32 public immutable I_PRICE_FEED_ID;
     uint256 public immutable I_STALE_AFTER;
     IPyth public immutable I_PYTH_CONTRACT;
+
+    constructor(PythOracleConfig memory config) {
+        I_PRICE_FEED_ID = config.priceFeedId;
+        I_STALE_AFTER = config.staleAfter;
+        I_PYTH_CONTRACT = config.pythContract;
+
+        emit Construction(msg.sender, config);
+    }
 
     function _price() internal virtual override returns (uint256) {
         PythStructs.Price memory price = I_PYTH_CONTRACT.getPriceNoOlderThan(I_PRICE_FEED_ID, I_STALE_AFTER);
